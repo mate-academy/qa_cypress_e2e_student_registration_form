db44c4b1f58c626bef7c61746b3ab418241bcba4
y.kostenko <y.kostenko@mate.academy>
2021-05-06 14:28:58 +0300

    homework 3

---
+++ cypress/integration/cypressActions.spec.js
@@ -0,0 +1,20 @@
+/// <reference types='cypress' />
+
+// Basic level:
+// 1. Fill all fields in forms except "picture" 
+// 2. Click on [Submit] button
+// 3. Validate inputed data in modal window
+// Site: https://demoqa.com/automation-practice-form
+
+// Advanced level:
+// Check next test cases:
+// 1. Pagination 
+// 2. Rows count selection
+// 3. Add new worker
+// 4. Delete worker
+// 5. Delete all worker
+// 6. Find worker in search field and edit it
+// 7. Validate data in worker row after creating worker
+// 8. Check search by all column values
+
+// https://demoqa.com/webtables
