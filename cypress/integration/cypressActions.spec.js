eea602e87d68ef021f662508efbccfaaa06cde0a
Dmytro Kuznietsov <kdvgym@gmail.com>
2021-08-11 14:53:54 +0300

    basic hw

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -18,3 +18,33 @@
 // 8. Check search by all column values
 
 // https://demoqa.com/webtables
+
+describe('Fill fields', () => {
+    before('', () => {
+            cy.visit('/');
+        });
+    it('fill all fields', () => {
+        cy.get('#firstName').type('Eric');
+        cy.get('#lastName').type('Cartman');
+        cy.get('#userEmail').type('e.cartman@sp.com');
+        cy.get('[for=gender-radio-1]').click();
+        cy.get('#userNumber').type('0123456789');
+        cy.get('#dateOfBirthInput').type('{selectall}').type('01 Dec 1999{enter}');
+        cy.get('.subjects-auto-complete__input').type('e{enter}').type('co{enter}');
+        cy.get('[for=hobbies-checkbox-3]').click();
+        cy.get('#currentAddress').type('Some Text about South Park');
+        cy.get('.css-1wa3eu0-placeholder').contains('Select State').type('u{enter}');
+        cy.get('.css-1wa3eu0-placeholder').contains('Select City').type('a{enter}');
+        cy.get('#submit').click();
+        cy.get('tbody td').contains('Eric').should('exist');
+        cy.get('tbody td').contains('Cartman').should('exist');
+        cy.get('tbody td').contains('e.cartman@sp.com').should('exist');
+        cy.get('tbody td').contains('Male').should('exist').click();
+        cy.get('tbody td').contains('0123456789').should('exist');
+        cy.get('tbody td').contains('01 December,1999').should('exist');
+        cy.get('tbody td').contains('English, Computer Science').should('exist');
+        cy.get('tbody td').contains('Music').should('exist');
+        cy.get('tbody td').contains('Some Text about South Park').should('exist');
+        cy.get('tbody td').contains('Uttar Pradesh Agra').should('exist');
+    });
+}); 
