19509ae8e0e02a7e46e5b062620862ed6951437e
Olena Vdovychenko <ena.noname@gmail.com>
2021-08-11 15:48:35 +0300

    Basic level solution

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -1,4 +1,4 @@
-/// <reference types='cypress' />
+/// <reference types="cypress" />
 
 // Basic level:
 // 1. Fill all fields in forms except "picture" 
@@ -18,3 +18,29 @@
 // 8. Check search by all column values
 
 // https://demoqa.com/webtables
+
+describe('',() => {
+  before('', () => {
+    cy.visit('/automation-practice-form');
+  });
+  
+  it('', () => {
+    cy.get('#firstName').type(`Olena`);
+    cy.get('#lastName').type(`Vdovychenko`);
+    cy.get('#userEmail').type(`ena.noname@gmail.com`);
+    cy.get('.custom-control.custom-radio.custom-control-inline').contains('Female').click();
+    cy.get('#userNumber').type(`0122033456`);
+    cy.get('#dateOfBirthInput').type(`{selectall}`).type('12 September 1997{enter}');
+    cy.get('#subjectsContainer').type(`QA`);
+    cy.get('.custom-control-label').contains('Sports').click();
+    cy.get('.custom-control-label').contains('Reading').click();
+    cy.get('.custom-control-label').contains('Music').click();
+    cy.get('#currentAddress').type(`85 Sunny Street`);
+    cy.get('#state').click().contains('Rajasthan').click();
+    cy.get('#city').click().contains('div', 'Jaipur').click({force: true});
+
+    cy.get('#submit').click();
+
+    cy.contains('tbody tr', 'Student Name').should('contain.text', 'Olena Vdovychenko');
+  });
+});
