ca3c315200acb748487ab4a0d8042f7df63e906b
AlexandraBozhan <sashkabozhan1997@gmail.com>
2021-08-11 23:19:12 +0300

    Save

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -1,5 +1,41 @@
 /// <reference types='cypress' />
 
+describe('',() => {
+    beforeEach(() => {
+      cy.visit('https://demoqa.com/automation-practice-form');
+    });
+
+    it('Successful login', () => {
+       cy.get('#firstName').type('Alexandra');
+       cy.get('#lastName').type('Bozhan');
+       cy.get('#userEmail').type('oleksandrabozhan@gmail.com');
+       cy.get('[for="gender-radio-2"]').type('{selectall}');
+       cy.get('#userNumber').type('0680826223');
+       cy.get('#dateOfBirthInput').type('{selectall}').type('05 April 1997{enter}');
+       cy.get('#subjectsWrapper').type('QA');
+       cy.get('.custom-control-label').contains('Reading').click();
+       cy.get('#currentAddress-wrapper').type('Earth'); 
+       cy.get('#state').click().contains('Rajasthan').click();
+       cy.get('#city').click().contains('div', 'Jaipur').click({force: true});
+
+       cy.get('#submit').click();
+    
+       cy.get('#example-modal-sizes-title-lg').should('contain', 'Thanks for submitting the form');
+
+      });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 // Basic level:
 // 1. Fill all fields in forms except "picture" 
 // 2. Click on [Submit] button
