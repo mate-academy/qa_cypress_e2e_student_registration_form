39775ca581d2d129e8038a360153337c62884e4c
Maksym Didenko <astakhov.mark@gmail.com>
2021-08-12 00:24:53 +0300

    HW

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -18,3 +18,36 @@
 // 8. Check search by all column values
 
 // https://demoqa.com/webtables
+
+describe('', () => {
+    before('Opening site', () => {
+      cy.visit('automation-practice-form');
+    });
+ 
+    it('Some test', () => {
+        cy.get('#firstName').type(`Maksym`);
+        cy.get('#lastName').type(`Didenko`);
+        cy.get('#userEmail').type(`astakhov.mark@gmail.com`);
+        cy.get('.custom-control-label').contains('Male').click();
+        cy.get('#userNumber').type(`1234567890`);
+        cy.get('#dateOfBirthInput').type(`{selectall}`).type('21 February 1995{enter}');
+        cy.get('#subjectsContainer').type('Co').contains('div', 'Computer Science').click({force: true});
+        cy.get('.custom-control-label').contains('Sports').click();
+        cy.get('.custom-control-label').contains('Reading').click();
+        cy.get('.custom-control-label').contains('Music').click();
+        cy.get('#currentAddress').type(`Ukraine, Odesa`);
+        cy.get('#state').click().contains('Haryana').click();
+        cy.get('#city').click().contains('div', 'Karnal').click({force: true});
+ 
+        cy.get('#submit').click();
+ 
+        cy.contains('tbody tr', 'Student Name').should('contain.text', 'Maksym Didenko');
+        cy.contains('tbody tr', 'Student Email').should('contain.text', 'astakhov.mark@gmail.com');
+        cy.contains('tbody tr', 'Gender').should('contain.text', 'Male');
+        cy.contains('tbody tr', 'Date of Birth').should('contain.text', '21 February,1995');
+        cy.contains('tbody tr', 'Subjects').should('contain.text', 'Computer Science');
+        cy.contains('tbody tr', 'Hobbies').should('contain.text', 'Sports, Reading, Music');
+        cy.contains('tbody tr', 'Address').should('contain.text', 'Ukraine, Odesa');
+        cy.contains('tbody tr', 'State and City').should('contain.text', 'Haryana Karnal');
+      });
+    });
