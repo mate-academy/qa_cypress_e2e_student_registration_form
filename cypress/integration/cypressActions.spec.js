8ad928057e0f71e9a9456b79ffc9b328ac85d3c8
Pavlo Starynskyi <starinskiy@gmail.com>
2021-08-11 14:17:38 +0300

    basic level

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -18,3 +18,38 @@
 // 8. Check search by all column values
 
 // https://demoqa.com/webtables
+
+describe('Create and validate new student', () => {
+
+    before('Visit web-site', () => {
+      cy.visit('/automation-practice-form');
+    });
+
+    it('Create new student', () => {
+      cy.get('#firstName').type('Ian');
+      cy.get('#lastName').type('Gillan');
+      cy.get('#userEmail').type('deep@purple.com');
+      cy.get('[for="gender-radio-1"]').click();
+      cy.get('#userNumber').type('0101011968');
+      cy.get('#dateOfBirthInput').type('{selectall}').type('19 August 1945{enter}');
+      cy.get('#subjectsInput').type('Computer Science{enter}');
+      cy.get('[for="hobbies-checkbox-3"]').click();
+      cy.get('#currentAddress').type('London, Great Britain');
+      cy.get('#state').type('NCR{enter}');
+      cy.get('#city').type('Delhi{enter}');
+      cy.get('#submit').click();
+    });
+
+    it('Check if credentials are correct', () => {
+      cy.contains('tbody tr', 'Student Name').should('contain.text', 'Ian Gillan');
+      cy.contains('tbody tr', 'Student Email').should('contain.text', 'deep@purple.com');
+      cy.contains('tbody tr', 'Gender').should('contain.text', 'Male');
+      cy.contains('tbody tr', 'Mobile').should('contain.text', '0101011968');
+      cy.contains('tbody tr', 'Date of Birth').should('contain.text', '19 August,1945');
+      cy.contains('tbody tr', 'Subjects').should('contain.text', 'Computer Science');
+      cy.contains('tbody tr', 'Hobbies').should('contain.text', 'Music');
+      cy.contains('tbody tr', 'Address').should('contain.text', 'London, Great Britain');
+      cy.contains('tbody tr', 'State and City').should('contain.text', 'NCR Delhi');
+      });
+
+  });
