ff4838c546d1375d0ddefc09bfbeb17811b7ee08
SmHanna <hannayours2014@gmail.com>
2021-08-12 19:46:22 +0300

    cypress_evening4

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -1,6 +1,6 @@
 /// <reference types='cypress' />
 
-describe('', () => {
+describe('cypressTest', () => {
   before('', () => {
   cy.visit ('https://demoqa.com/automation-practice-form');
   });
@@ -12,16 +12,24 @@ it('Basic level', ()=> {
   cy.get('#userEmail').type('hannayours@gmail.com');
   cy.get('.custom-control-label').contains('Female').click(); 
   cy.get('#userNumber').type('4587475877');
-  cy.get('#dateOfBirthInput').type(`{selectall}`).type('4 march 1956{enter}');
+  cy.get('#dateOfBirthInput').type(`{selectall}`).type('24 march 1956{enter}');
   cy.get('#subjectsContainer').type('His').contains('div', 'History').click({force: true});
   cy.get('.custom-control-label').contains('Reading').click();
   cy.get('#currentAddress').type('Ukraine, Kyiv');
   cy.get('#stateCity-wrapper').click().contains('Haryana').click();
   cy.get('#city').click().contains('div', 'Karnal').click({force: true}); 
   cy.get('#submit').click();
-});
-});
 
+  cy.contains('tbody tr', 'Student Name').should('contain.text', 'hanna smirnova');
+        cy.contains('tbody tr', 'Student Email').should('contain.text', 'hannayours@gmail.com');
+        cy.contains('tbody tr', 'Gender').should('contain.text', 'Female');
+        cy.contains('tbody tr', 'Date of Birth').should('contain.text', '24 march 1956');
+        cy.contains('tbody tr', 'Subjects').should('contain.text', 'History');
+        cy.contains('tbody tr', 'Hobbies').should('contain.text', 'Sports, Reading, Music');
+        cy.contains('tbody tr', 'Address').should('contain.text', 'Ukraine, Kyiv');
+        cy.contains('tbody tr', 'State and City').should('contain.text', 'Haryana Karnal');
+      });
+    });
 
 // Basic level:
 // 1. Fill all fields in forms except "picture" 
