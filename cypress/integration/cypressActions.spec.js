6f7358578bd0106f66f0438d15d3d83e13592fab
AlexandraBozhan <sashkabozhan1997@gmail.com>
2021-08-13 20:33:19 +0300

    Solution Fixed

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -1,56 +1,33 @@
-/// <reference types='cypress' />
+/// <reference types="cypress" />
+describe ('basic level', () => {
 
-describe('',() => {
-    beforeEach(() => {
+  before('open site', () => {
       cy.visit('https://demoqa.com/automation-practice-form');
+  })
+  it('Register', () => {
+      cy.get('#firstName').type('Lilian');
+      cy.get('#lastName').type('Voinich');
+      cy.get('#userEmail').type('lilian@run.com');
+      cy.get('[for="gender-radio-3"]').click();
+      cy.get('#userNumber').type('0101010101');
+      cy.get('#dateOfBirthInput').type('{selectall}').type('08 August 1958{enter}');
+      cy.get('#subjectsWrapper').type('QA{enter}');
+      cy.get('[for="hobbies-checkbox-3"]').click({force: true});
+      cy.get('#currentAddress').type('Earth');
+      cy.get('#state').type('Rajasthan{enter}');
+      cy.get('#city').type('Jaipur{enter}');
+      cy.get('#submit').click();
     });
 
-    it('Successful login', () => {
-       cy.get('#firstName').type('Alexandra');
-       cy.get('#lastName').type('Bozhan');
-       cy.get('#userEmail').type('oleksandrabozhan@gmail.com');
-       cy.get('[for="gender-radio-2"]').type('{selectall}');
-       cy.get('#userNumber').type('0680826223');
-       cy.get('#dateOfBirthInput').type('{selectall}').type('05 April 1997{enter}');
-       cy.get('#subjectsWrapper').type('QA');
-       cy.get('.custom-control-label').contains('Reading').click();
-       cy.get('#currentAddress-wrapper').type('Earth'); 
-       cy.get('#state').click().contains('Rajasthan').click();
-       cy.get('#city').click().contains('div', 'Jaipur').click({force: true});
-
-       cy.get('#submit').click();
-    
-       cy.get('#example-modal-sizes-title-lg').should('contain', 'Thanks for submitting the form');
-
+  it('Validated registration', () => {
+      cy.contains('tbody tr', 'Student Name').should('contain.text', 'Lilian Voinich');
+      cy.contains('tbody tr', 'Student Email').should('contain.text', 'lilian@run.com');
+      cy.contains('tbody tr', 'Gender').should('contain.text', 'Other');
+      cy.contains('tbody tr', 'Mobile').should('contain.text', '0101010101');
+      cy.contains('tbody tr', 'Date of Birth').should('contain.text', '08 August,1958');
+      cy.contains('tbody tr', 'Subjects').should('contain.text', 'QA');
+      cy.contains('tbody tr', 'Hobbies').should('contain.text', 'Music');
+      cy.contains('tbody tr', 'Address').should('contain.text', 'Earth');
+      cy.contains('tbody tr', 'State and City').should('contain.text', 'Rajasthan Jaipur');
       });
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-// Basic level:
-// 1. Fill all fields in forms except "picture" 
-// 2. Click on [Submit] button
-// 3. Validate inputed data in modal window
-// Site: https://demoqa.com/automation-practice-form
-
-// Advanced level:
-// Check next test cases:
-// 1. Pagination 
-// 2. Rows count selection
-// 3. Add new worker
-// 4. Delete worker
-// 5. Delete all worker
-// 6. Find worker in search field and edit it
-// 7. Validate data in worker row after creating worker
-// 8. Check search by all column values
-
-// https://demoqa.com/webtables
+});
