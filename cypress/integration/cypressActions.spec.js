f43270d7418b32fe94658887ef8f85323cf38929
AlexandraBozhan <sashkabozhan1997@gmail.com>
2021-08-16 22:03:50 +0300

    Solution finally

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -10,8 +10,8 @@ describe ('basic level', () => {
       cy.get('#userEmail').type('lilian@run.com');
       cy.get('[for="gender-radio-3"]').click();
       cy.get('#userNumber').type('0101010101');
-      cy.get('#dateOfBirthInput').type('{selectall}').type('08 August 1958{enter}');
-      cy.get('#subjectsWrapper').type('QA{enter}');
+      cy.get('#dateOfBirthInput').type('{selectall}').type('05 April 1997{enter}');
+      cy.get('#subjectsWrapper').type('Computer Science{enter}');
       cy.get('[for="hobbies-checkbox-3"]').click({force: true});
       cy.get('#currentAddress').type('Earth');
       cy.get('#state').type('Rajasthan{enter}');
@@ -24,8 +24,8 @@ describe ('basic level', () => {
       cy.contains('tbody tr', 'Student Email').should('contain.text', 'lilian@run.com');
       cy.contains('tbody tr', 'Gender').should('contain.text', 'Other');
       cy.contains('tbody tr', 'Mobile').should('contain.text', '0101010101');
-      cy.contains('tbody tr', 'Date of Birth').should('contain.text', '08 August,1958');
-      cy.contains('tbody tr', 'Subjects').should('contain.text', 'QA');
+      cy.contains('tbody tr', 'Date of Birth').should('contain.text', '05 April,1997');
+      cy.contains('tbody tr', 'Subjects').should('contain.text', 'Computer Science');
       cy.contains('tbody tr', 'Hobbies').should('contain.text', 'Music');
       cy.contains('tbody tr', 'Address').should('contain.text', 'Earth');
       cy.contains('tbody tr', 'State and City').should('contain.text', 'Rajasthan Jaipur');
