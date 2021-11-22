59c5d41ee0c910f589f2e914c211b17126ae779d
Anastasiia Talybova <talybova.an@gmail.com>
2021-11-22 19:13:37 +0200

    Homework 35 [Talybova Anastasiia]

--- cypress/integration/cypressActions.spec.js
+++ cypress/integration/cypressActions.spec.js
@@ -18,3 +18,167 @@
 // 8. Check search by all column values
 
 // https://demoqa.com/webtables
+
+let user;
+
+describe('Basic', () => {
+
+  before(function () {
+    cy.task("newUser").then((object) => {
+        user = object;
+    });
+  });
+
+  it('Student Registration Form', () => {
+    cy.visit('https://demoqa.com/automation-practice-form');
+    cy.get('#firstName').type(user.firstName);
+    cy.get('#lastName').type(user.lastName);
+    cy.get('#userEmail').type(user.userEmail);
+    cy.get('[for="gender-radio-3"]').click({force: true});
+    cy.get('#userNumber').type(user.userNumber);
+    cy.get('#dateOfBirthInput').type('{selectall}').type('20 April 1990{enter}');
+    cy.get('#subjectsContainer').type('Biology{enter}');
+    cy.get('#hobbies-checkbox-1').click({force: true});
+    cy.get('#hobbies-checkbox-2').click({force: true});
+    cy.get('#hobbies-checkbox-3').click({force: true});
+    cy.get('[placeholder="Current Address"]').type(user.address);
+    cy.get('.col-md-4.col-sm-12 div.css-2b097c-container').click();
+    cy.get('#react-select-3-option-0').click();
+    cy.get('#city .css-yk16xz-control').click();
+    cy.get('#react-select-4-option-1').click();
+    cy.get('#submit').click();
+  })
+
+  it('Validate inputed data in modal window in Student Registration Form', () => {
+    cy.contains('tr', 'Student Name').should('contain', user.firstName);
+    cy.contains('tr', 'Student Email').should('contain', user.userEmail);
+    cy.contains('tr', 'Gender').should('contain', 'Other');
+    cy.contains('tr', 'Mobile').should('contain', user.userNumber);
+    cy.contains('tr', 'Date of Birth').should('contain', '20 April');
+    cy.contains('tr', 'Subjects').should('contain', 'Biology');
+    cy.contains('tr', 'Hobbies').should('contain', 'Sports');
+    cy.contains('tr', 'Address').should('contain', user.address);
+    cy.contains('tr', 'State and City').should('contain', 'NCR');
+  })
+})
+
+// Advanced level:
+describe('Advanced', () => {
+
+  before(function () {
+    cy.task("newUser").then((object) => {
+        user = object;
+    });
+  });
+
+  it('Open page', () => {
+    cy.visit('https://demoqa.com/webtables');
+  })
+
+  it('Add new worker', () => {
+    cy.visit('https://demoqa.com/webtables');
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName);
+    cy.get('#lastName').type(user.lastName);
+    cy.get('#userEmail').type(user.userEmail);
+    cy.get('#age').type(user.age);
+    cy.get('#salary').type(user.salary);
+    cy.get('#department').type('Biology{enter}');
+
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName1);
+    cy.get('#lastName').type(user.lastName1);
+    cy.get('#userEmail').type(user.userEmail1);
+    cy.get('#age').type(user.age1);
+    cy.get('#salary').type(user.salary1);
+    cy.get('#department').type('Algebra{enter}');
+
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName2);
+    cy.get('#lastName').type(user.lastName2);
+    cy.get('#userEmail').type(user.userEmail2);
+    cy.get('#age').type(user.age2);
+    cy.get('#salary').type(user.salary2);
+    cy.get('#department').type('Geometry{enter}');
+
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName3);
+    cy.get('#lastName').type(user.lastName3);
+    cy.get('#userEmail').type(user.userEmail3);
+    cy.get('#age').type(user.age3);
+    cy.get('#salary').type(user.salary3);
+    cy.get('#department').type('Chemistry{enter}');
+  })
+
+  it('Validate data in worker row after creating worker', () => {
+    cy.contains('div.rt-td', user.firstName).should('contain', user.firstName);
+    cy.contains('div.rt-td', user.lastName).should('contain', user.lastName);
+    cy.contains('div.rt-td', user.userEmail).should('contain', user.userEmail);
+    cy.contains('div.rt-td', user.age).should('contain', user.age);
+    cy.contains('div.rt-td', user.salary).should('contain', user.salary);
+    cy.contains('div.rt-td', 'Biology').should('contain', 'Biology');
+
+    cy.contains('div.rt-td', user.firstName1).should('contain', user.firstName1);
+    cy.contains('div.rt-td', user.lastName1).should('contain', user.lastName1);
+    cy.contains('div.rt-td', user.userEmail1).should('contain', user.userEmail1);
+    cy.contains('div.rt-td', user.age1).should('contain', user.age1);
+    cy.contains('div.rt-td', user.salary1).should('contain', user.salary1);
+    cy.contains('div.rt-td', 'Algebra').should('contain', 'Algebra');
+
+    cy.contains('div.rt-td', user.firstName2).should('contain', user.firstName2);
+    cy.contains('div.rt-td', user.lastName2).should('contain', user.lastName2);
+    cy.contains('div.rt-td', user.userEmail2).should('contain', user.userEmail2);
+    cy.contains('div.rt-td', user.age2).should('contain', user.age2);
+    cy.contains('div.rt-td', user.salary2).should('contain', user.salary2);
+    cy.contains('div.rt-td', 'Geometry').should('contain', 'Geometry');
+
+    cy.contains('div.rt-td', user.firstName3).should('contain', user.firstName3);
+    cy.contains('div.rt-td', user.lastName3).should('contain', user.lastName3);
+    cy.contains('div.rt-td', user.userEmail3).should('contain', user.userEmail3);
+    cy.contains('div.rt-td', user.age3).should('contain', user.age3);
+    cy.contains('div.rt-td', user.salary3).should('contain', user.salary3);
+    cy.contains('div.rt-td', 'Chemistry').should('contain', 'Chemistry');
+  })
+
+  it('Rows count selection', () => {
+    cy.get('[aria-label="rows per page"]').select('5 rows').should('have.value', '5');
+  })
+
+  it('Pagination', () => {
+    cy.get('[aria-label="jump to page"]').type('{selectall}').type('2{enter}');
+  })
+
+  it('Rows count selection', () => {
+    cy.get('#searchBox').type('{selectall}').type(user.firstName1).type('{enter}');
+    cy.get('#searchBox').type('{selectall}').type(user.lastName1).type('{enter}');
+    cy.get('#searchBox').type('{selectall}').type(user.userEmail1).type('{enter}');
+    cy.get('#searchBox').type('{selectall}').type(user.age1).type('{enter}');
+    cy.get('#searchBox').type('{selectall}').type(user.salary1).type('{enter}');
+    cy.get('#searchBox').type('{selectall}').type('Algebra{enter}');
+  })
+
+  it('Find worker in search field and edit it', () => {
+    cy.get('#searchBox').type('{selectall}').type(user.firstName).type('{enter}');
+    cy.get('[title="Edit"]').click();
+    cy.contains('#registration-form-modal', 'Registration Form').should('exist');
+    cy.get('#firstName').type('{selectall}').type(user.firstName1);
+    cy.get('#submit').click();
+  })
+
+  it('Delete worker', () => {
+    cy.get('#searchBox').type('{selectall}').type(user.lastName3).type('{enter}');
+    cy.get('#delete-record-7').click();
+    cy.contains('div.rt-td', user.lastName3).should('not.exist');
+  })
+
+  it('Delete worker', () => {
+    cy.get('#searchBox').type('{selectall}').type('{backspace}');
+    cy.get('#delete-record-1').click();
+    cy.get('#delete-record-2').click();
+    cy.get('#delete-record-3').click();
+    cy.get('#delete-record-4').click();
+    cy.get('#delete-record-5').click();
+    cy.get('#delete-record-6').click();
+  })
+
+})
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,42 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const { random } = require("faker");
+const faker = require("faker");
+
+module.exports = (on, config) => {
+    on("task", {
+        newUser() {
+            user = {
+              firstName: faker.name.firstName() + `${Math.round(Math.random() * 10000)}`,
+              lastName: faker.name.lastName() + `${Math.round(Math.random() * 10000)}`,
+              userEmail: faker.internet.email(),
+              userNumber: `${Math.round(Math.random() * 1000000000)}`,
+              address: Math.random().toString(36).substr(2, 15),
+              age: `${Math.round(Math.random() * 100)}`,
+              salary: `${Math.round(Math.random() * 10000)}`,
+
+              firstName1: faker.name.firstName(),
+              lastName1: faker.name.lastName(),
+              userEmail1: faker.internet.email(),
+              age1: `${Math.round(Math.random() * 100)}`,
+              salary1: `${Math.round(Math.random() * 10000)}`,
+
+              firstName2: faker.name.firstName(),
+              lastName2: faker.name.lastName(),
+              userEmail2: faker.internet.email(),
+              age2: `${Math.round(Math.random() * 100)}`,
+              salary2: `${Math.round(Math.random() * 10000)}`,
+
+              firstName3: faker.name.firstName(),
+              lastName3: faker.name.lastName(),
+              userEmail3: faker.internet.email(),
+              age3: `${Math.round(Math.random() * 100)}`,
+              salary3: `${Math.round(Math.random() * 10000)}`,
+            };
+            return user;
+
+        },
+    });
+};
