e4096fcb1956c3d71f23887e8fd4d01a250b16a6
Roman Stas <romastas1996@gmail.com>
2022-05-29 11:10:50 +0300

    new doc for user

---
+++ cypress/support/user_data.js
@@ -0,0 +1,14 @@
+
+const userData = {
+  userName: "Franco",
+  lastName: "Dior",
+  email: 'Franco@mail.com',
+  phone: "1234567891",
+  dateBirth: "22 March,1999",
+  subject: "Maths",
+  adress: "Evergreen Teras 747",
+  state: "Haryana",
+  city: "Karnal",
+}
+
+export default userData;
