6440e3b1e2362f787a895e1768e9ebf8f4ae4aed
claimsecond <claim.second@gmail.com>
2022-07-10 18:53:38 +0300

    solution

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,12 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+// import './commands';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
 
 // Alternatively you can use CommonJS syntax:
-// require('./commands')
+require('./commands');
+
