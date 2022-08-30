16e4176efb600548c70010151e38f0b3d3715f7e
Maxim Ozernoy <Jbc404@gmail.com>
2022-08-30 18:14:52 +0300

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    baseUrl: 'https://demoqa.com/automation-practice-form/#'
-   
-  },
-})
+  viewportWidth: 1600,
+  viewportHeight: 1200,
+  baseUrl: 'https://demoqa.com/automation-practice-form/#'
+  }
+});
--- cypress/e2e/cypressActions.cy.js
+++ cypress/e2e/cypressActions.cy.js
@@ -2,8 +2,8 @@
 
 describe('Practice Form', () => {
   before(() => {
-    cy.visit('/')
-    cy.viewport(1600, 1200)
+    cy.visit('/');
+    //cy.viewport(1600, 1200);
   });
 
   it('Should register a student filling the required fields', () => {
@@ -23,7 +23,7 @@ describe('Practice Form', () => {
     .type('0998887766');
    
     cy.get('[id="dateOfBirthInput"]')
-    .type('{selectAll}').type('23 May 1900').click();
+    .type('{selectAll}').type('23 May 1900{enter}');
 
     cy.get('[class="subjects-auto-complete__control css-yk16xz-control"]')
     .click('right')
@@ -47,10 +47,14 @@ describe('Practice Form', () => {
     cy.get('[id="submit"]')
     .click();
 
+    cy.contains('[class="modal-body"]', 'John Johnson');
     cy.contains('[class="modal-body"]', 'Johnson@gmail.com');
-
     cy.contains('[class="modal-body"]', 'Male');
-
+    cy.contains('[class="modal-body"]', '0998887766');
     cy.contains('[class="modal-body"]', '23 May,1900');
+    cy.contains('[class="modal-body"]', 'Physics, Computer Science');
+    cy.contains('[class="modal-body"]', 'Sports, Music');
+    cy.contains('[class="modal-body"]', 'There is no available address. Check your permission key.');
+    cy.contains('[class="modal-body"]', 'Uttar Pradesh Agra');
   });
 });
