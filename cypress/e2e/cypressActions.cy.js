77d3c3a96c4c1eda605dd364239547c05fe64846
Anastasiia Lisova <nastya.krush@gmail.com>
2022-10-07 20:35:02 +0300

    Cypress_third fixed

--- cypress/e2e/cypressActions.cy.js
+++ cypress/e2e/cypressActions.cy.js
@@ -8,56 +8,74 @@ describe('Student Registration Form', () => {
   });
 
   it('should accept filling all fields in form except "picture"', () => {
-    const {firstName, lastName, email, mobileNumber, birthDate, currentAdress} = generateStudent ();
+    const {firstName, lastName, email, mobileNumber, currentAdress} = generateStudent ();
 
-    cy.get('[id="firstName"]')
+    cy.get('#firstName')
       .type(firstName);
-    cy.get('[id=lastName]')
+    cy.get('#lastName')
       .type(lastName);
-    cy.get('[id="userEmail"]')
+    cy.get('#userEmail')
       .type(email);
-    cy.get('[id="genterWrapper"]')
-      .contains('.custom-control-label', 'Male')
+    cy.get('[for="gender-radio-1"]')
       .click();
-    cy.get('[id="userNumber"]')
+    cy.get('#userNumber')
       .type(mobileNumber);
 
-    cy.get('[id="dateOfBirthInput"]')
+    cy.get('#dateOfBirthInput')
       .click();
     cy.get('[class="react-datepicker__year-select"]')
       .select('1994');
     cy.get('[class="react-datepicker__day react-datepicker__day--003"]')
       .click();
 
-    cy.get('[id="subjectsContainer"]')
-      .type('C');
-    cy.get('[id="react-select-2-option-0"]')
-      .click();
+    cy.get('#subjectsContainer')
+      .type('Chemistry'+'{enter}');
 
-      
-    cy.contains('label', 'Sports')
-      .click();
-    cy.contains('label', 'Reading')
-      .click();
-    cy.contains('label', 'Music')
-      .click();
+    cy.get('#hobbies-checkbox-1')
+      .click({force: true});
+    cy.get('#hobbies-checkbox-2')
+      .click({force: true});
+    cy.get('#hobbies-checkbox-3')
+      .click({force: true});
 
     cy.get('[id="currentAddress"]')
       .type(currentAdress);
-    cy.get('[id="state"]')
-      .click();
-    cy.get('[id="react-select-3-option-0"]')
-      .click();
-    cy.contains('div', 'Select City')
-      .click();
-    cy.get('[class=" css-26l3qy-menu"]')
-      .click();
+    cy.get('#state')
+      .type('NCR'+'{enter}');
+    cy.get('#city')
+      .type('Delhi'+'{enter}');
 
     cy.contains('button', 'Submit')
       .click({force: true});
 
-    cy.get('[class="modal-content"]')
-      .should('contain.text', 'Thanks for submitting the form')
+    cy.contains('.modal-header', 'Thanks for submitting the form');
+
+    cy.contains('tr', 'Student Name')
+      .should('contain', firstName + ' ' + lastName);
+
+    cy.contains('tr', 'Student Email')
+      .should('contain', email);
+
+    cy.contains('tr', 'Gender')
+      .should('contain', 'Male');
+
+    cy.contains('tr', 'Mobile')
+      .should('contain', mobileNumber);
+
+    cy.contains('tr', 'Date of Birth')
+      .should('contain', '03 October,1994');
+
+    cy.contains('tr', 'Subjects')
+      .should('contain', 'Chemistry');
+
+    cy.contains('tr', 'Hobbies')
+      .should('contain', 'Sports, Reading, Music');
+
+    cy.contains('tr', 'Address')
+      .should('contain', currentAdress);
+
+    cy.contains('tr', 'State and City')
+      .should('contain', 'NCR Delhi');
   });
 
 });
