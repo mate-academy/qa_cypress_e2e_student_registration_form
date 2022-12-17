aee84ec69ebe32f960129c9e93d02c381cc08955
Roman Paliy <paliygamingua@gmail.com>
2022-12-17 23:12:19 +0200

    mySolutionWithFixedConfig

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+    },
+    baseUrl: 'https://demoqa.com/automation-practice-form/'
+  },
+});
