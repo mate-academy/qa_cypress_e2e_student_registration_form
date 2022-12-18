b4b4a12dd8f92cf5e794b6efb51a63f4cd999651
Oleksandra Vlasyuk <oleksandra.vl30@gmail.com>
2022-12-18 13:19:19 +0200

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,59 @@
 /// <reference types='cypress' />
+const { generateUser } = require('../support/generate');
 
 describe('Student Registration page', () => {
-  before(() => {
-    
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/automation-practice-form')
   });
 
-  it('', () => {
-    
+  it('user should fill all fields in forms> except "picture"', () => {
+    const { firstName, lastName, email, mobile } = generateUser();
+
+    cy.get('#firstName')
+      .type(firstName);
+
+    cy.get('#lastName')
+      .type(lastName);
+
+    cy.get('#userEmail')
+      .type(email);
+
+    cy.get('#gender-radio-1')
+      .click({force: true});
+
+    cy.get('#userNumber')
+      .type(mobile);
+
+    cy.get('[id = "dateOfBirthInput"]')
+      .type('{selectAll}' + '01 Nov 1991')
+      .click();
+
+    cy.get('.subjects-auto-complete__value-container')
+      .click({force: true})
+      .type('r' + '{enter}');
+
+    cy.get('[id="hobbies-checkbox-1"]')
+      .click({force: true});
+
+    cy.get('[id="currentAddress"]')
+      .click({force: true})
+      .type('Current Address');
+
+    cy.get('#state')
+      .click({force: true})
+      .type('u' + '{enter}');
+
+    cy.get('#city')
+      .click({force: true})
+      .type('g' + '{enter}');
+
+    cy.contains('#submit', 'Submit')
+      .click({force: true});
+
+    cy.contains('#example-modal-sizes-title-lg', 'Thanks for submitting the form');
+
+    cy.contains('tbody > tr > td', `${firstName} ${lastName}`);
+
+    cy.contains('tbody > tr > td', `${email}`);
   });
 });
