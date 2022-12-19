27bca2650a5824f67a157f31a24bb72cb53edbdf
YUrii Kachur <yurii.kachurr@gmail.com>
2022-12-19 08:45:26 +0200

    registration_solution_2

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -3,15 +3,14 @@ import {faker} from '@faker-js/faker';
  const firstName = faker.name.firstName();
  const lastName = faker.name.lastName();
  const streetAddress = faker.address.streetAddress()  
+ const number = faker.datatype.number({ min: 1000000 })
 describe('Student Registration page', () => {
   before(() => {
     cy.visit('/automation-practice-form')
   });
 
   it('Student should be registered form by filling all fields in forms except "picture"', () => {
-    
-    const randomNumberGenerator = Math.random(7).toString().slice(3)
-    
+ 
     cy.get('[placeholder="First Name"]')
       .type(firstName);
     
@@ -25,7 +24,7 @@ describe('Student Registration page', () => {
       .click({force: true});
 
     cy.get('[placeholder="Mobile Number"]')
-      .type(`063${randomNumberGenerator}`);
+      .type(`063${number}`);
 
     cy.get('[id="dateOfBirthInput"]')
       .click()
@@ -34,34 +33,61 @@ describe('Student Registration page', () => {
       .type('{enter}');
     
     cy.get('.subjects-auto-complete__value-container')
-      .type('e')
+      .type('e');
       
     cy.get('[id="react-select-2-option-0"]')
-      .click()
+      .click();
 
     cy.get('[id="currentAddress"]')
-      .type(`${streetAddress}`)
+      .type(`${streetAddress}`);
 
     cy.get('[id="hobbies-checkbox-1"]')
       .click({ force: true});
 
     cy.get('[class=" css-1wa3eu0-placeholder"]')
       .contains('Select State')
-      .click({force: true})
+      .click({force: true});
     
     cy.get('[id="react-select-3-option-0"]')
       .contains('NCR')
-      .click({force: true})
+      .click({force: true});
     
     cy.get('[class=" css-1wa3eu0-placeholder"]')
       .contains('Select City')
       .click({force: true})
-      .type('Delhi')
+      .type('Delhi');
       
     cy.get('[id="submit"]')
-      .click({ force: true })
+      .click({ force: true });
     
     cy.get('[id="example-modal-sizes-title-lg"]')
-      .should('contain', 'Thanks for submitting the form')
+      .should('contain', 'Thanks for submitting the form');
+
+    cy.contains('tr', 'Student Name')  
+      .should('contain', `${firstName} ${lastName}`);
+
+    cy.contains('tr', 'Student Email')  
+      .should('contain', `${firstName}@mail.com`);
+
+    cy.contains('tr', 'Gender')  
+      .should('contain', 'Male');  
+
+    cy.contains('tr', 'Mobile')  
+      .should('contain', `063${number}`);
+
+    cy.contains('tr', 'Date of Birth')  
+      .should('contain', '01 May,2000'); 
+
+    cy.contains('tr', 'Subject')  
+      .should('contain', 'English'); 
+
+    cy.contains('tr', 'Hobbies')  
+      .should('contain', 'Sport'); 
+
+    cy.contains('tr', 'Address')  
+      .should('contain', `${streetAddress}`); 
+
+    cy.contains('tr', 'State and City')  
+      .should('contain', 'NCR Delhi');
   });
 });
