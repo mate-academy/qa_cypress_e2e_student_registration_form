9a41ecf3aa2833f1f883c4e15d3393309e350251
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2023-01-23 13:15:24 -0600

    test3 resolved 2

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -3,13 +3,13 @@
 const { generateUser } = require("../support/generate");
 
 describe('Student Registration page', () => {
-  before(() => {
-   cy.visit('/') 
 
-  });
+ 
 
   it('it should allow a new user to register', () => {
     const {firstName, lastName, email, phoneNumber, adresss} = generateUser();
+
+    cy.visit('/') 
 
     cy.findByIdNumber('firstName')
       .type(firstName)
@@ -26,40 +26,40 @@ describe('Student Registration page', () => {
     cy.findByIdNumber('userNumber')
       .type(phoneNumber)
 
-      cy.findByIdNumber('dateOfBirthInput')
-        .click()
+    cy.findByIdNumber('dateOfBirthInput')
+      .click()
 
-      cy.get('.react-datepicker__month-select').select('April');  
+    cy.get('.react-datepicker__month-select').select('April');  
       
-      cy.get('.react-datepicker__year-select').select('1988')
-      cy.get('.react-datepicker__day--013').click();
+    cy.get('.react-datepicker__year-select').select('1988')
+    cy.get('.react-datepicker__day--013').click();
     
-      cy.get(".subjects-auto-complete__value-container")
-        .click()
-        .type('Computer Science')
+    cy.get(".subjects-auto-complete__value-container")
+      .click()
+      .type('Computer Science')
 
-      cy.get('#react-select-2-option-0').contains('Computer Science')
+    cy.get('#react-select-2-option-0').contains('Computer Science')
       .click()
       
-      cy.get('.custom-control-label').contains('Sports').click()
+    cy.get('.custom-control-label').contains('Sports').click()
 
-      cy.get('#currentAddress')
-        .type(adresss)
+    cy.get('#currentAddress')
+      .type(adresss)
 
-      cy.get(':nth-child(4) > .group-header > .header-wrapper').click()
+    cy.get(':nth-child(4) > .group-header > .header-wrapper').click()
 
-      cy.findByIdNumber('state').click()
-      cy.findByIdNumber('react-select-3-option-0').click()
+    cy.findByIdNumber('state').click()
+    cy.findByIdNumber('react-select-3-option-0').click()
      
-      cy.findByIdNumber('city').click()
-      cy.findByIdNumber('react-select-4-option-0')
+    cy.findByIdNumber('city').click()
+    cy.findByIdNumber('react-select-4-option-0')
       .click()
       
-      cy.get('#submit').contains('button', 'Submit')
-        .click()
+    cy.get('#submit').contains('button', 'Submit')
+      .click()
       
 
-      cy.get('#example-modal-sizes-title-lg').should('contain.text', 'Thanks for submitting the form')
+    cy.get('#example-modal-sizes-title-lg').should('contain.text', 'Thanks for submitting the form')
         
   });
 });
