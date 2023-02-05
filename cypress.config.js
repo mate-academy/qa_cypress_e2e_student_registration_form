cf4896b62feb9f4f718c599d30b1108775ec5ed3
Kyrylo Stadnyk <faust.ogr8@gmail.com>
2023-02-05 18:59:58 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
+    baseUrl: 'https://demoqa.com/automation-practice-form'
   },
+
+  viewportWidth: 1280,
+  viewportHeight: 720,
 })
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,50 @@
 /// <reference types='cypress' />
 
+const { generateUser } = require("../support/generate");
+
 describe('Student Registration page', () => {
-  before(() => {
-    
+  beforeEach(() => {
+    cy.visit('/');
   });
 
-  it('', () => {
+  it('should display entered data', () => {
+    const { firstname, lastname, email, phone, address } = generateUser();
+
+    cy.get('#firstName')
+      .type(firstname);
     
+    cy.get('#lastName')
+      .type(lastname);
+    
+    cy.get('#userEmail')
+      .type(email);
+    
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(1)')
+      .click();
+    
+    cy.get('#userNumber')
+      .type(phone);
+    
+    cy.get('#currentAddress')
+      .type(address);
+    
+    cy.get('#submit')
+      .click({ force: true })
+    
+    cy.get('tbody > :nth-child(1) > :nth-child(2)')
+      .should('contain', `${firstname} ${lastname}`);
+    
+    cy.get('tbody > :nth-child(2) > :nth-child(2)')
+      .should('contain', `${email}`);
+    
+    cy.get('tbody > :nth-child(3) > :nth-child(2)')
+      .should('contain', `Male`);
+    
+    cy.get('tbody > :nth-child(4) > :nth-child(2)')
+      .should('contain', `${phone}`);
+    
+    cy.get('tbody > :nth-child(9) > :nth-child(2)')
+      .should('contain', `${address}`);
   });
 });
+
