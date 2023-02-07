92540caa56ebd037a3194501554a6f347cd5676b
Yaroslav <adolas666@gmail.com>
2023-02-07 15:14:21 +0200

    removed requests loggin

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,8 +14,18 @@
 // ***********************************************************
 
 Cypress.on('uncaught:exception', () => {
-  return false
+  return false;
 });
+
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+};
 
 // Import commands.js using ES2015 syntax:
 import './commands'
