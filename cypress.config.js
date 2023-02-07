df65d2338a98dac021779bdf499edbb228cb4cc8
Artem-Kyryliuk <artemiykyryliuk@gmail.com>
2023-02-07 19:17:55 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://demoqa.com/automation-practice-form",
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
     },
+    
   },
 })
