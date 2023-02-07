2245a37fdf96a63ba20b685e18879f39f009cda1
Bohdan Zaborovets <bogdan2013live@gmail.com>
2023-02-07 20:38:33 +0200

    Registration page testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1320
   },
 })
