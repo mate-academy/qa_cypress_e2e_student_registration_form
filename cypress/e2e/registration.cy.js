fbfe6a31ed051684efc4ae53b0a3f6b197628904
Khrystyna Sabat <satttinka@gmail.com>
2023-02-08 16:13:35 +0100

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,74 @@
 /// <reference types='cypress' />
+const { generateUser } = require('../support/generate');
+const { firstName, lastName, email, mobile, birthday, address } = generateUser();
 
 describe('Student Registration page', () => {
-  before(() => {
-    
+  beforeEach(() => {
+    cy.visit('/')
   });
 
-  it('', () => {
-    
+  it('should', () => {
+    cy.get('#firstName')
+      .type(firstName);
+
+    cy.get('#lastName')
+      .type(lastName);
+
+    cy.get('#userEmail')
+      .type(email);
+
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(2) > .custom-control-label')
+      .click();
+
+    cy.get('#userNumber')
+      .type(mobile);
+
+    cy.get('#dateOfBirthInput')
+      .type(`{selectAll} ${birthday} {enter}`);
+
+    cy.get('.subjects-auto-complete__value-container')
+      .type('Computer science{enter}');
+
+    cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label')
+      .click();
+
+    cy.get('#currentAddress')
+      .type(address);
+
+      cy.get('#state')
+        .type('NCR{Enter}');
+
+      cy.get('#city')
+        .type('Delhi{Enter}');
+  
+      cy.get('#submit')
+        .click();
+
+      cy.contains('tr', 'Student Name')
+        .should('contain', firstName + ' ' + lastName);
+
+      cy.contains('tr', 'Student Email')
+        .should('contain', email);
+
+      cy.contains('tr', 'Gender')
+        .should('contain', 'Female');
+
+      cy.contains('tr', 'Mobile')
+        .should('contain', mobile);
+
+      cy.contains('tr', 'Date of Birth')
+        .should('contain', ' ');
+
+      cy.contains('tr', 'Subjects')
+        .should('contain', 'Computer Science');
+
+      cy.contains('tr', 'Hobbies')
+        .should('contain', `Sports`);
+   
+      cy.contains('tr', 'Address')
+        .should('contain', `${address}`);
+
+      cy.contains('tr', 'State and City')
+        .should('contain', 'NCR Delhi');
   });
 });
