16b6a342408cc82528f0de09f2ea94b4b0f49484
Lesia Okseniuk <hrushkachereshnia@gmail.com>
2023-02-08 15:45:45 +0200

    Solution

---
+++ cypress/support/generate.js
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker';
+
+function generateUser() {
+  const username = faker.internet.userName();
+  const email = faker.internet.email();
+  const phone = faker.phone.number('##########');
+  const address = faker.address.streetAddress();
+
+  return { username, email, phone, address };
+} 
+
+module.exports = { generateUser };
