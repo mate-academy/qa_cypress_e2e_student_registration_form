b1e0555d42b237a17cf907e79c46ce74e4f5af15
Ievgenii <e.kolodeev@gmail.com>
2023-02-08 17:35:58 +0100

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,29 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1440,
+    viewportWidth: 1440,
     setupNodeEvents(on, config) {
+      on("task", {
+        generateUser() {
+          randomIndex = Math.floor(Math.random() * 3);
+          genders = ['Male', 'Female', 'Other'];
+          hobbies = ['Reading', 'Sport', 'Music'];
+          userEmail = faker.internet.email().toLowerCase();
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: userEmail,
+            phoneNumber: faker.phone.phoneNumber('##########'),
+            gender: genders[randomIndex],
+            hobby: hobbies[randomIndex],
+            address: faker.address.streetAddress()
+          };
+        },
+      });
     },
   },
-})
+});
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,41 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
+  let user;
+
   before(() => {
-    
+    cy.visit('/');
+    cy.task('generateUser').then(generateUser => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
-    
-  });
+  it('should register a new user', () => {
+    cy.findByPlaceholder('First Name')
+      .type(user.firstName);
+    cy.findByPlaceholder('Last Name')
+      .type(user.lastName);
+    cy.findByPlaceholder('name@example.com')
+      .type(user.email);
+    cy.contains('.custom-control-label', user.gender)
+      .click();
+    cy.findByPlaceholder('Mobile Number')
+      .type(user.phoneNumber);
+    cy.get('#dateOfBirthInput')
+      .type('{selectAll}07 FEB 1990{enter}');
+    cy.get('.subjects-auto-complete__value-container')
+      .type('En{enter}' + 'Ar{enter}' + 'Mat{enter}');
+    cy.contains('.custom-control-label', user.hobby)
+      .click();
+    cy.findByPlaceholder('Current Address')
+      .type(user.address);
+    cy.get('#state')
+      .type('{downArrow}{downArrow}{enter}');
+    cy.get('#city')
+      .type('{downArrow}{enter}');
+    cy.get('#submit')
+      .click();
+    cy.contains('tr', 'Student Name')
+      .should('contain', `${user.firstName} ${user.lastName}`);
+    });
 });
