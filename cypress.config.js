2b3a1dc498db26100030c4ce30032fd8564927f6
Lesia Okseniuk <hrushkachereshnia@gmail.com>
2023-02-08 14:56:34 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,14 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const faker = require('@faker-js/faker');
+
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
-    },
-  },
-})
+
+  },    
+},
+});
