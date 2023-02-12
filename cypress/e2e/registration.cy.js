0b2e1d14de3f61167bdbc87a54f3839f6ac1837f
Bobuliak Viktor <bobuliakviktor@gmail.com>
2023-02-12 10:55:33 +0200

    Student registration

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,30 @@
 /// <reference types='cypress' />
 
+
 describe('Student Registration page', () => {
+  let user;
   before(() => {
-    
+    cy.visit('/');
+    cy.task('generateUser').then(generateUser => {
+      user = generateUser;
+    })
   });
 
-  it('', () => {
-    
+  it('should register a new user', () => {
+    cy.findByPlaceholder('First Name').type(user.firstName);
+    cy.findByPlaceholder('Last Name').type(user.lastName);
+    cy.findByPlaceholder('name@example.com').type(user.email);
+    cy.contains('.custom-control-label', user.gender).click();
+    cy.findByPlaceholder('Mobile Number').type(user.phoneNumber);
+    cy.get('#dateOfBirthInput').type('{selectAll}24 Dec 2021{enter}');
+    cy.get('.subjects-auto-complete__value-container').type('Ma{enter}');
+    cy.contains('.custom-control-label', user.hobby).click();
+    cy.findByPlaceholder('Current Address').type(user.address);
+    cy.get('#state').type('{downArrow}{enter}');
+    cy.get('#city').type('{downArrow}{enter}');
+    cy.get('#submit').click();
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .and('contain', user.lastName);
   });
 });
