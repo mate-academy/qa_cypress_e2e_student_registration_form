789f0d4d9d5ed04fdf491d3937d046cf7cbea93f
Max Nikiforov <maxxnikiforov@gmail.com>
2023-04-06 18:33:34 +0700

    tests added

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,26 +1,29 @@
 /// <reference types='cypress' />
+import { generateuser } from "../support/generateuser";
 
 describe('Student Registration page', () => {
   beforeEach('', () => {
-    cy.viewport(550, 750) // viewport to 550px x 750px because the page does not scroll
+    cy.viewport(550, 750)
     cy.visit('https://demoqa.com/automation-practice-form');
   }); 
 
   it('should be possible to register', () => {
+    const{ firstname, lastname, email, address, phone } = generateuser();
+
     cy.get('#firstName')
-      .type('name')
+      .type(firstname)
 
       cy.get('#lastName')
-      .type('last_name')
+      .type(lastname)
 
       cy.get('#userEmail')
-      .type('name@mail.com')
+      .type(email)
 
       cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label')
       .click()
 
       cy.get('#userNumber')
-      .type('1234567890')
+      .type(phone)
 
       cy.get('#dateOfBirthInput')
         .click()
@@ -34,7 +37,7 @@ describe('Student Registration page', () => {
       cy.get('label[for="hobbies-checkbox-2"]').click()
 
       cy.get('[id="currentAddress"]')
-        .type('Vietnam')
+        .type(address)
 
       cy.get('#state').click().type('NCR{enter}');
       cy.get('#city').click().type('Delhi{enter}');
@@ -45,10 +48,64 @@ describe('Student Registration page', () => {
         .should('contain.text', 'Thanks for submitting the form')
 
         cy.get('.modal-content')
-        .should('contain',`name last_name`)
-        .should('contain', 'name@mail.com')
-        .should('contain', '1234567890')
+        .should('contain', `${firstname} ${lastname}`)
+        .should('contain', email)
+        .should('contain', phone)
         .should('contain', 'English')
-        .should('contain', 'Vietnam');
+        .should('contain', address)
+  });
+
+  it('should throw an error if not all required fields filled', () => {
+    const{ firstname } = generateuser();
+
+    cy.get('#firstName')
+      .type(firstname)
+
+    cy.contains('button', 'Submit').click()
+
+    cy.get('#lastName')
+      .should('have.css', 'border-color', 'rgb(220, 53, 69)')
+
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label')
+      .should('have.css', 'border-color', 'rgb(220, 53, 69)')
+
+    cy.get('#userNumber')
+      .should('have.css', 'border-color', 'rgb(220, 53, 69)')
+  });
+
+  it('should be possible to choose several subjects and only one gender', () => {
+    const{ firstname, lastname, email, phone } = generateuser();
+
+    cy.get('#firstName')
+      .type(firstname)
+
+      cy.get('#lastName')
+      .type(lastname)
+
+      cy.get('#userEmail')
+      .type(email)
+
+      cy.get('#genterWrapper > .col-md-9 > :nth-child(2) > .custom-control-label')
+      .click()
+
+      cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label')
+      .click()
+
+      cy.get('#userNumber')
+      .type(phone)
+
+      cy.get('#subjectsInput').type('English{enter}');
+      cy.get('#subjectsInput').type('Physics{enter}');
+      cy.get('#subjectsInput').type('Chemistry{enter}');
+
+      cy.contains('button', 'Submit').click()
+
+      cy.get('.modal-content')
+        .should('contain', 'English')
+        .should('contain', 'Physics')
+        .should('contain', 'Chemistry')
+
+        .should('contain', 'Male')
+        .should('not.visible', 'Female')
   });
 });
