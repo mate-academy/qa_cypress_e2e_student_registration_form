b91b0e64f6857b501b1573c0469f79d07a1ee6a7
Julia Stadnyk <stadnik271@gmail.com>
2023-04-12 17:43:52 +0300

    student_registration_form[Julia_Stadnyk]

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,25 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+            on("task", {
+                generateUser() {
+                  let randNum = Math.floor(Math.random() * 2) + 1;
+                  let genders = ['Male', 'Female', 'Other'];
+                  let hobbies = ['Sports', 'Reading', 'Music'];
+                  return {
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.firstName(),
+                    email: faker.internet.email(),
+                    gender: genders[randNum],
+                    // date: faker.date.birthdate(),
+                    hobby: hobbies[randNum],
+                    address: faker.address.city(),
+                  };
+                },
+            });
+        },
     },
-  },
-})
+});
