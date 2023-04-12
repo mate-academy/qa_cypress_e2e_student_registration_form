bbfb994ca5f76132362499f62088234ab2184288
HalynaKomar <komarg007@gmail.com>
2023-04-12 19:12:24 +0300

    Halyna Komar Student Registration Form

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,28 @@
 /// <reference types='cypress' />
 
-describe('Student Registration page', () => {
-  before(() => {
-    
-  });
 
-  it('', () => {
-    
+describe('Student Registration page', () => {
+  let user;
+  before(() => {
+    cy.visit('/')
+    cy.task('generateUser').then(generatedUser => {
+      user = generatedUser
+    });
+  });
+  it('should allow to register a new user', () => {
+
+    cy.findByPlaceholder('First Name').type(user.firstName)
+    cy.findByPlaceholder('Last Name').type(user.lastName);
+    cy.findByPlaceholder('name@example.com').type(user.email)
+    cy.contains('.custom-control-label', user.gender).click()
+    cy.findByPlaceholder('Mobile Number').type(user.phoneNumber)
+    cy.get('#dateOfBirthInput').type('{selectAll} + user.birthDate + {enter}')
+    cy.get('.subjects-auto-complete__value-container').type('phy{enter}')
+    cy.contains('.custom-control-label', user.hobby).click()
+    cy.findByPlaceholder('Current Address').type(user.address)
+    cy.get('#state').type('{downArrow}{enter}')
+    cy.get('#city').type('{downArrow}{enter}')
+    cy.get('#submit').click()
+    cy.contains('tr', 'Student Name').should('contain', user.firstName).and('contain', user.lastName)
   });
 });
