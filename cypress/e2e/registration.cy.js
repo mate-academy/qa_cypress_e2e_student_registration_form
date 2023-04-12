4ed320ec81ba2c2b2ccd4e3071eea36411f2e6fb
Bohdan Karpenko <19bogdank99@gmail.com>
2023-04-12 13:39:09 +0300

    Homework

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,44 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
+  let user;
+
   before(() => {
-    
+    cy.visit('/');
+    cy.task('generateUser').then(generatedUser => {
+      user = generatedUser;
+    });
+
   });
 
-  it('', () => {
-    
+  it('should allow to register new user', () => {
+    cy.viewport(550, 750);
+
+    cy.findByPlaceholder('First Name')
+      .type(user.firstName);
+
+    cy.findByPlaceholder('Last Name')
+     .type(user.lastName);
+
+    cy.get('#userEmail').type(user.email);
+
+    cy.findByType('radio').check(user.gender, { force: true });
+
+    cy.findByPlaceholder('Mobile Number')
+      .type(user.mobileNumber);
+
+    cy.get('#dateOfBirthInput').type('{selectAll}'+user.birthDate+'{enter}');
+
+    cy.get('.subjects-auto-complete__value-container').type('ma{enter} ch{enter} his{enter}');
+
+    cy.contains('.custom-control-label', user.hobby).click();
+
+    cy.get('#currentAddress').type(user.adress);
+
+    cy.get('#state').type('ut{enter}');
+
+    cy.get('#city').type('me{enter}');
+
+    cy.get('#submit').click();
   });
 });
