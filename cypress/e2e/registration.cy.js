271ee2ce9e5664a901be53a34c38752281ccf161
Alina Muzyka <mamajalina@gmail.com>
2023-04-13 18:04:20 +0300

    home work 3 [Alina Muzyka]

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -12,18 +12,24 @@ describe('Student Registration page', () => {
 
   it('should allow to register a new user', () => {
 
-    cy.findByPlaceholder('First Name').type(user.firstName)
+    cy.findByPlaceholder('First Name').type(user.firstName);
     cy.findByPlaceholder('Last Name').type(user.lastName);
-    cy.findByPlaceholder('name@example.com').type(user.email)
-    cy.contains('.custom-control-label', user.gender).click()
-    cy.findByPlaceholder('Mobile Number').type(user.phoneNumber)
-    cy.get('#dateOfBirthInput').type('{selectAll} + user.birthDate + {enter}')
-    cy.get('.subjects-auto-complete__value-container').type('phy{enter}')
-    cy.contains('.custom-control-label', user.hobby).click()
-    cy.findByPlaceholder('Current Address').type(user.address)
-    cy.get('#state').type('{downArrow}{enter}')
-    cy.get('#city').type('{downArrow}{enter}')
-    cy.get('#submit').click()
-    cy.contains('tr', 'Student Name').should('contain', user.firstName).and('contain', user.lastName)
+    cy.findByPlaceholder('name@example.com').type(user.email);
+    cy.contains('.custom-control-label', user.gender).click();
+    cy.findByPlaceholder('Mobile Number').type(user.phoneNumber);
+    cy.get('#dateOfBirthInput').type('{selectAll} + user.birthDate + {enter}');
+    cy.get('.subjects-auto-complete__value-container').type('phy{enter}');
+    cy.contains('.custom-control-label', user.hobby).click();
+    cy.findByPlaceholder('Current Address').type(user.address);
+    cy.get('#state').type('{downArrow}{enter}');
+    cy.get('#city').type('{downArrow}{enter}');
+    cy.get('#submit').click();
+    cy.contains('#example-modal-sizes-title-lg', 'Thanks for submitting the form').should('exist');
+    cy.get('tbody > :nth-child(1) > :nth-child(2)').should('contain', `${user.firstName} ${user.lastName}`);
+    cy.get('tbody > :nth-child(2) > :nth-child(2)').should('contain', user.email);
+    cy.get('tbody > :nth-child(3) > :nth-child(2)').should('contain', user.gender);
+    cy.get('tbody > :nth-child(4) > :nth-child(2)').should('contain', user.mobileNumber);
+    cy.get('tbody > :nth-child(7) > :nth-child(2)').should('contain', user.hobby);
+    cy.get('tbody > :nth-child(9) > :nth-child(2)').should('contain', user.adress);
   });
 });
