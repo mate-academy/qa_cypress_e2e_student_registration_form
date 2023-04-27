7a19a944c70b4dce147ac39189fd219b1bb1a33b
Dima Gaplik <124164297+DimaGaplik@users.noreply.github.com>
2023-04-27 14:23:32 +0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,8 +27,4 @@
 Cypress.Commands.add('findByPlaceholder', (placeholder) => {
     cy.get(`[placeholder="${placeholder}"]`)
 });
-
-// Cypress.Commands.add('findByType', (type) => {
-//     cy.get(`[type="${type}"]`);
-//   });
   
