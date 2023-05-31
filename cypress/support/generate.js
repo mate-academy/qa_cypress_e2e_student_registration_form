493466207d694ee683922b428aba72503f3462e6
Olga Akimenko <olga.katsion@gmail.com>
2023-05-31 18:46:44 +0300

    registration

---
+++ cypress/support/generate.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('cypress')
+const faker = require('faker');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1280,
+    setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          genders = ['Male', 'Female', 'Other'];
+          hobbies = ['Sports', 'Reading', 'Music'];
+          subjects = ['English', 'Chemistry', 'Commerce', 'Economics', 'Arts'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(genders),
+            phoneNumber: faker.phone.phoneNumber('##########'),
+            subject: faker.random.arrayElement(subjects),
+            hobby: faker.random.arrayElement(hobbies),
+            address: faker.address.streetAddress(),
+          };
+        },
+      });
+    },
+  },
+})
