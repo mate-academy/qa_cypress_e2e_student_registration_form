3c1ac155c468d2ff48de8629d9a73a7eba3b6f0a
OlSkrypnikov <Al.skrypnikov@gmail.com>
2023-05-31 22:11:43 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,34 @@
 const { defineConfig } = require('cypress')
+const faker = require('faker')
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1980,
+    baseUrl: 'https://demoqa.com/automation-practice-form' , 
     setupNodeEvents(on, config) {
+      on("task", {
+        generateUser() {
+          genders = ['Male', 'Female', 'Other'];
+          subjects = ['Chemistry', 'English', 'Computer Science'];
+          hobbies = ['Sports', 'Reading', 'Music'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement (genders),
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            birth: {
+                    year: Math.floor(1960 + Math.random() * 60),
+                    month: Math.floor(Math.random() * 12),
+                    day: Math.floor(10 + Math.random() * 28),
+            },
+            subject: faker.random.arrayElement (subjects),
+            hobby: faker.random.arrayElement (hobbies),
+            address: faker.address.streetAddress (),
+          };
+        },
+      });
     },
   },
-})
+});
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,69 @@
 /// <reference types='cypress' />
+import faker from 'faker';
 
 describe('Student Registration page', () => {
-  before(() => {
-    
+  let user;
+before(() => {
+    cy.visit("https://demoqa.com/automation-practice-form");
+    cy.task("generateUser").then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
+  it('Should register new student', () => {
     
+    cy.findByPlaceholder('First Name')
+      .type(user.firstName);
+    cy.findByPlaceholder('Last Name')
+      .type(user.lastName);
+    cy.get('#userEmail')
+      .type(user.email);
+    cy.get(`[value=${user.gender}]`)
+      .click({ force: true});
+    cy.findByPlaceholder('Mobile Number')
+      .type(user.mobileNumber);
+    cy.get('#dateOfBirthInput')
+      .click();
+    cy.pickDate('year-select')
+      .select(`${user.birth.year}`);
+    cy.pickDate('month-select')
+      .select(`${user.birth.month}`);
+    cy.pickDate('day')
+      .contains(user.birth.day)
+      .click();
+    cy.get('.subjects-auto-complete__value-container')
+      .type(user.subject + '{enter}');
+    cy.contains('.custom-control-label', user.hobby)
+      .click();
+    cy.findByPlaceholder('Current Address')
+      .type(user.address);
+    cy.contains('Select State')
+      .type('{downArrow}{enter}');
+    cy.contains('Select City')
+      .type('{downArrow}{enter}');
+    cy.get('#submit')
+      .click();
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .and('contain', user.lastName);
+    cy.contains('tr', 'Student Email')
+      .should('contain', user.email);
+    cy.contains('tr', 'Gender')
+      .should('contain', user.gender);
+    cy.contains('tr', 'Mobile')
+      .should('contain', user.mobileNumber);
+    cy.contains('tr', 'Date of Birth')
+      .should('contain', user.birth.year)
+      .and('contain', user.birth.day);
+    cy.contains('tr', 'Subjects')
+      .should('contain', user.subject);
+    cy.contains('tr', 'Hobbies')
+      .should('contain', user.hobby);
+    cy.contains('tr', 'Address')
+      .should('contain', user.address);
+    cy.contains('tr', 'State and City')
+      .should('contain', 'Uttar Pradesh')
+      .and('contain', 'Lucknow'); 
+      });
   });
-});
+
