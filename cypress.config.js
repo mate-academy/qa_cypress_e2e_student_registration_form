e94721b9d4da0518756b2b74bc7c53b2dee85ebb
Ivan Fomenko <fomenko1ivan@gmail.com>
2023-05-31 18:19:28 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,35 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
+      on("task", {
+        generateUser() {
+          genders = ['Male', 'Female', 'Other'];
+          subjects = ['Math', 'Arts', 'Biology', 'Physics', 'Commerce', 'Economics'];
+          hobbies = ['Sports', 'Reading', 'Music']
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(genders),
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            birth: {
+              year: Math.floor(1950 + Math.random() * 60),
+              month: Math.floor(Math.random() * 12),
+              day: Math.floor(Math.random() * 16),
+            },
+            subject: faker.random.arrayElement(subjects),
+            hobby: faker.random.arrayElement(hobbies),
+            address: faker.address.streetAddress(),
+          };
+        },
+      });
     },
   },
-})
+});
+
