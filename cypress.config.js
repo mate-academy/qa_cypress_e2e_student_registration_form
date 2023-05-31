41f02040209cb4348cda5f567ab2299f43882dde
Artem <protiv55@gmail.com>
2023-05-31 17:54:58 +0300

    Solurion

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,34 @@
 const { defineConfig } = require('cypress')
+const faker = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1200,
+    viewportWidth: 1680,
     setupNodeEvents(on, config) {
+      on("task",{
+        generateUser() {
+          genders = ['Female', 'Male', 'Other'];
+          subjects = ['English', 'Chemistry', 'Computer Science'];
+          hobbies = ['Music', 'Sports', 'Reading'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(genders),
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            birth: {
+              year: Math.floor(1960 + Math.random() * 60),
+              month: Math.floor(Math.random() * 12),
+              day: Math.floor(10 + Math.random() * 15),
+            },
+            subject: faker.random.arrayElement(subjects),
+            hobby: faker.random.arrayElement(hobbies),
+            address: faker.address.streetAddress(),
+          };
+        },
+      });
     },
   },
-})
+});
