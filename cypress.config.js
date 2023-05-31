9219848c15f635896da684b4c1ad49f379a5c1f6
NataliaSavenets <savenets.natalia@gmail.com>
2023-05-31 17:20:06 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,31 @@
 const { defineConfig } = require('cypress')
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1980,
+    
     setupNodeEvents(on, config) {
+      on("task",{
+        generateUser() {
+          genders = ['Male', 'Female', 'Other'];
+          subjects = ['English', 'Chemistry', 'Biology'];
+          hobbies = ['Sports', 'Reading', 'Music'];
+
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(genders),
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            subject: faker.random.arrayElement(subjects),
+            hobby: faker.random.arrayElement(hobbies),
+            address: faker.address.streetAddress()
+          };
+        },
+      });
     },
   },
-})
+});
+
