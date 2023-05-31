34f037b858d858992de17d0d6c21c6f782291b56
Anton Kalnohuz <akalnoguz@gmail.com>
2023-05-31 17:18:29 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,33 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
+      on("task",{
+        generateUser() {
+          const genders = ['Female', 'Male', 'Other'];
+          const subjects = ['English', 'Chemistry', 'Computer Science', 'Art'];
+          const hobbies = ['Music', 'Sports', 'Reading'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(genders),
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            birth: {
+              year: Math.floor(1900 + Math.random() * 100),
+              month: Math.floor(Math.random() * 12),
+              day: Math.floor(10 + Math.random() * 15),
+            },
+            subject: faker.random.arrayElement(subjects),
+            hobby: faker.random.arrayElement(hobbies),
+            address: faker.address.streetAddress(),
+          };
+        },
+      });
     },
   },
 })
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,96 @@
 /// <reference types='cypress' />
 
-describe('Student Registration page', () => {
-  before(() => {
-    
+describe('Students Registration page', () => {
+  let user;
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/automation-practice-form');
+    cy.task('generateUser')
+      .then(generateUser => {
+        user = generateUser;
+      })
   });
 
-  it('', () => {
+  it('should fill in the form and assert inputted data', () => {
+    // Fill in the  first name field
+    cy.get('#firstName').type(user.firstName);
+
+     // Fill in the last name field
+    cy.get('#lastName').type(user.lastName);
+
+    // Fill in the email field
+    cy.get('#userEmail').type(user.email);
+
+    // Select gender
+    cy.get(`[value = ${user.gender}]`)
+    .click({ force: true });
+
+    cy.contains('.custom-control-label', user.gender)
+    .click();
+
+    // Fill in the mobile number field
+    cy.get('#userNumber').type(user.mobileNumber);
+
+    // Fill in the date of birth field
+    cy.get('#dateOfBirthInput')
+    .click();
+
+    cy.pickDate('year-select')
+    .select(`${user.birth.year}`);
+
+    cy.pickDate('month-select')
+    .select(`${user.birth.month}`);
+
+    cy.pickDate('day')
+    .contains(user.birth.day)
+    .click();
+
+    // Select subjects
+    cy.get('#subjectsInput').type(user.subject).type('{enter}');
+
+    // Select hobbies
+    cy.contains('.custom-control-label', user.hobby)
+    .click()
+
+    // Fill in the current address field
+    cy.get('#currentAddress').type(user.address);
+
+    // Select state and city
+    cy.contains('Select State').type('{downArrow}{enter}');
+    cy.contains('Select City').type('{downArrow}{enter}');
+  
+
+    // Submit the form
+    cy.get('#submit').click();
+
+    // Assert inputted data in the modal window
+    cy.contains('tr', 'Student Name')
+    .should('contain', user.firstName)
+    .and('contain', user.lastName);
+
+    cy.contains('tr', 'Student Email')
+    .should('contain', user.email);
+    
+    cy.contains('tr', 'Gender')
+    .should('contain', user.gender);
+
+    cy.contains('tr', 'Mobile')
+    .should('contain', user.mobileNumber);
+
+    cy.contains('tr', 'Date of Birth')
+    .should('contain', user.birth.day)
+    .and('contain', user.birth.year);
+
+    cy.contains('tr', 'Subjects')
+    .should('contain', user.subject);
+
+    cy.contains('tr', 'Hobbies')
+    .should('contain', user.hobby);
+
+    cy.contains('tr', 'Address')
+    .should('contain', user.address);
+    
+    cy.contains('tr', 'State and City')
+    .should('contain.text', 'Uttar Pradesh Lucknow');
     
   });
 });
