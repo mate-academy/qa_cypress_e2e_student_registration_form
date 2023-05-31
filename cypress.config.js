e4544f458a6ef5dd2fbe6f56bade7bf2674bfc62
Oleksandr Ohryzko <sasha.ogrizko@gmail.com>
2023-05-31 20:17:23 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,25 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
     setupNodeEvents(on, config) {
+      on("task", {
+        generateUser() {
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(['Male', 'Female', 'Other']),
+            mobileNumber: faker.phone.phoneNumber('###########'),
+            subjects: faker.random.arrayElement(['Physics', 'English', 'Chemistry']),
+            hobbies: faker.random.arrayElement(['Sports', 'Reading', 'Music']),
+            address: faker.address.streetAddress(),
+          }
+        }
+      }
+      )
     },
   },
 })
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,45 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
+  let user;
+
   before(() => {
-    
+    cy.visit('/') ;
+    cy.task('generateUser')
+      .then(generateUser =>{
+        user = generateUser;
+
+      })     
   });
 
-  it('', () => {
+  it('should register a new student', () => {
     
+    cy.get('#firstName')
+      .type(user.firstName); 
+    cy.get('#lastName')
+      .type(user.lastName);
+    cy.get('#userEmail')
+      .type(user.email);
+    cy.contains('.custom-control-label', user.gender)
+      .click();   
+    cy.get('#userNumber')
+      .type(user.mobileNumber);
+    cy.get('#dateOfBirthInput')
+      .type('{selectall} 23 Feb 2000' + '{enter}');
+    cy.get('.subjects-auto-complete__value-container')
+      .type(user.subjects + '{enter}');
+    cy.contains('.custom-control-label', user.hobbies)
+      .click();
+    cy.get('#currentAddress')
+      .type(user.address);
+    cy.contains('Select State')
+      .type('{downArrow}{enter}');
+     cy.contains('Select City')
+      .type('{downArrow}{enter}');
+    cy.get('#submit')
+      .click({force : true});
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .and('contain', user.lastName);
   });
 });
