501d07b4484518f69acec10d4cba2eedbf7aee98
emmmcr <maximandreev99@gmail.com>
2023-05-31 17:24:30 +0300

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,60 @@
 /// <reference types='cypress' />
 
+const { generateUser } = require('../support/generate');
+
 describe('Student Registration page', () => {
   before(() => {
-    
+    cy.visit('https://demoqa.com/automation-practice-form');
   });
 
-  it('', () => {
+  it('should fill all the fields and submit the form', () => {
+    const user = generateUser();
     
+    cy.get('#firstName')
+      .type(user.firstName);
+    cy.get('#lastName')
+      .type(user.lastName);
+    cy.get('#userEmail')
+      .type(user.email);
+    cy.contains('.custom-control-label', user.gender)
+      .click();
+    cy.get('#userNumber')
+      .type(user.phone);
+    cy.get('#dateOfBirthInput')
+      .type('{selectAll} 22 Nov 1967');
+    cy.get('.subjects-auto-complete__value-container')
+      .type(user.subjects + '{enter}');
+    cy.contains('.custom-control-label', user.hobby)
+      .click();
+    cy.get('#currentAddress')
+      .type(user.address);
+    cy.contains('Select State')
+      .type('{downArrow}{enter}');
+    cy.contains('Select City')
+      .type('{downArrow}{enter}');
+    cy.get('#submit')
+      .click();
+
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .and('contain', user.lastName);
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName + ' ' + user.lastName);
+    cy.contains('tr', 'Student Email')
+      .should('contain', user.email);
+    cy.contains('tr', 'Gender')
+      .should('contain', user.gender);
+    cy.contains('tr', 'Mobile')
+      .should('contain', user.phone);
+    cy.contains('tr', 'Date of Birth')
+      .should('contain', '22 November,1967');
+    cy.contains('tr', 'Subjects')
+      .should('contain', user.subjects);
+    cy.contains('tr', 'Hobbies')
+      .should('contain', user.hobby);
+    cy.contains('tr', 'Address')
+      .should('contain', user.address);
+    cy.contains('tr', 'State and City')
+      .should('contain', 'Uttar Pradesh Lucknow');
   });
 });
