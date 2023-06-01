7bf626b1a4a28431b9c4300ed9e7f2ea0098f286
Vitalii Kaminskyi <kaminsky0321@gmail.com>
2023-06-01 15:05:44 +0300

    Re-request review

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,38 @@
-/// <reference types='cypress' />
+const { defineConfig } = require('cypress')
+const faker = require('faker');
 
-describe('Student Registration page', () => {
-  before(() => {
-    
-  });
+const genders = ['Female', 'Male', 'Other'];
+const subjects = ['English', 'Chemistry', 'Computer Science'];
+const hobbies = ['Music', 'Sports', 'Reading'];
 
-  it('', () => {
-    
-  });
-});
+module.exports = defineConfig({
+  e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1980,
+    setupNodeEvents(on, config) {
+      on("task",{    
+
+        generateUser() {
+          genders;
+          subjects;
+          hobbies;
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: faker.random.arrayElement(genders),
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            birth: {
+              year: Math.floor(1960 + Math.random() * 60),
+              month: Math.floor(Math.random() * 12),
+              day: Math.floor(10 + Math.random() * 15),
+            },
+            subject: faker.random.arrayElement(subjects),
+            hobby: faker.random.arrayElement(hobbies),
+            address: faker.address.streetAddress(),
+          };
+        },
+      });
+    },
+  },
+})
