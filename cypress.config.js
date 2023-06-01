a050d1036bb9db1eefedca7aa8be06b89dfb9c68
Iryna <novydom23@gmail.com>
2023-06-01 11:33:12 +0300

    Fix code

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('cypress')
 const faker = require('faker');
+genders = ['Male','Female', 'Other'];
+subjects =['Math', 'Physics', 'History'];
+hobbies = ['Sports','Reading', 'Music'];
 
 module.exports = defineConfig({
   e2e: {
@@ -9,9 +12,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on("task", {
         generateUser() {
-          genders = ['Male','Female', 'Other'];
-          subjects =['Math', 'Physics', 'History'];
-          hobbies = ['Sports','Reading', 'Music'];
           return {
             firstName: faker.name.firstName(),
             lastName: faker.name.lastName(),
