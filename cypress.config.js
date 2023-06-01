e59732944e80080f90572b49b2b02381d0f13c24
NataliaSavenets <savenets.natalia@gmail.com>
2023-06-01 10:19:21 +0300

    Solution edited

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,17 @@
 const { defineConfig } = require('cypress')
 const faker = require('faker');
+const genders = ['Male', 'Female', 'Other'];
+const subjects = ['English', 'Chemistry', 'Biology'];
+const hobbies = ['Sports', 'Reading', 'Music'];
 
 module.exports = defineConfig({
   e2e: {
     viewportHeight: 1080,
     viewportWidth: 1980,
-    
+
     setupNodeEvents(on, config) {
       on("task",{
         generateUser() {
-          genders = ['Male', 'Female', 'Other'];
-          subjects = ['English', 'Chemistry', 'Biology'];
-          hobbies = ['Sports', 'Reading', 'Music'];
-
           return {
             firstName: faker.name.firstName(),
             lastName: faker.name.lastName(),
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -13,47 +13,47 @@ describe('Student Registration page', () => {
 
   it('should provide an ability to register new account', () => {
     cy.findByPlaceholder('First Name')
-      .type(user.firstName)
+      .type(user.firstName);
     cy.findByPlaceholder('Last Name')
-      .type(user.lastName)
+      .type(user.lastName);
     cy.get('#userEmail')
-      .type(user.email)
+      .type(user.email);
     cy.contains('.custom-control-label', user.gender) 
       .click();
     cy.findByPlaceholder('Mobile Number')
-      .type(user.mobileNumber)
+      .type(user.mobileNumber);
     cy.get('#dateOfBirthInput')
-      .type('{selectall}07 Jun 1991'+ '{enter}')  
+      .type('{selectall}07 Jun 1991'+ '{enter}'); 
     cy.get('.subjects-auto-complete__value-container')
-      .type(user.subject + '{enter}')
+      .type(user.subject + '{enter}');
     cy.contains('.custom-control-label', user.hobby) 
       .click();
     cy.findByPlaceholder('Current Address')
-      .type(user.address)
+      .type(user.address);
     cy.contains('Select State')
-      .type('{downArrow}{enter}')
+      .type('{downArrow}{enter}');
     cy.contains('Select City')
-      .type('{downArrow}{enter}') 
-    cy.get('#submit').click({force: true})
+      .type('{downArrow}{enter}'); 
+    cy.get('#submit').click({force: true});
 
     cy.contains('tr','Student Name')
       .should('contain', user.firstName)
-      .and ('contain', user.lastName)
+      .and ('contain', user.lastName);
     cy.contains('tr','Student Email')
-      .should('contain', user.email)
+      .should('contain', user.email);
     cy.contains('tr','Gender')
-      .should('contain', user.gender)
+      .should('contain', user.gender);
     cy.contains('tr','Mobile')
-      .should('contain', user.mobileNumber)
+      .should('contain', user.mobileNumber);
     cy.contains('tr','Date of Birth')
-      .should('contain', '07 June,1991')
+      .should('contain', '07 June,1991');
     cy.contains('tr','Subjects')
-      .should('contain', user.subject)
+      .should('contain', user.subject);
     cy.contains('tr','Hobbies')
-      .should('contain', user.hobby)
+      .should('contain', user.hobby);
     cy.contains('tr','Address')
-      .should('contain', user.address)
+      .should('contain', user.address);
     cy.contains('tr','State and City')
-      .should('contain', 'Uttar Pradesh Lucknow')
+      .should('contain', 'Uttar Pradesh Lucknow');
   });
 });
