56c5a957c80a4a1d1b6485d5879b5893d9d72dbc
Roman Miskiv <molfargoods@gmail.com>
2023-07-17 17:34:51 +0300

    Solution Registration

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,29 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://demoqa.com/automation-practice-form",
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
-    }
-  }
-});
+      on("task", {
+        generateUser() {
+          randomIndex = Math.floor(Math.random() * 2);
+          genders = ['Male', 'Female', 'Other'];
+          hobbies = ['Sport', 'Reading', 'Music'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: genders[randomIndex],
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            hobby: hobbies[randomIndex],
+            address: faker.address.streetAddress()
+        
+          }
+        }
+      })
+    },
+  },
+})
