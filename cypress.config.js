10e42c154bbd10467d253bb7d9b0b3cff68cd3ec
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-07-17 17:16:39 +0300

    solution for registration form

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,28 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          const randomIndex = Math.floor(Math.random() * 2);
+          const genders = ['Male', 'Female', 'Other'];
+          const hobbies = ['Sports', 'Reading', 'Music'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: genders[randomIndex],
+            mobileNumber: faker.phone.phoneNumber('##########'),
+            hobby: hobbies[randomIndex],
+            address: faker.address.streetAddress()
+          };
+        }
+      });
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,67 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
+  let user;
   before(() => {
-
+    cy.visit('/');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
+  it('Should register a new  student', () => {
+    cy.findByPlaceholder('First Name').type(user.firstName);
 
+    cy.findByPlaceholder('Last Name').type(user.lastName);
+
+    cy.get('#userEmail').type(user.email);
+
+    cy.contains('.custom-control-label', user.gender).click();
+
+    cy.findByPlaceholder('Mobile Number').type(user.mobileNumber);
+
+    cy.get('#dateOfBirthInput')
+      .type('{selectall}23 Oct 2003{enter}');
+
+    cy.get('.subjects-auto-complete__value-container')
+      .type('en{enter}' + 'bio{enter}');
+
+    cy.contains('.custom-control-label', user.hobby).click();
+
+    cy.findByPlaceholder('Current Address').type(user.address);
+
+    cy.get('#state')
+      .type('{downarrow}{enter}');
+    cy.get('#city')
+      .type('{downarrow}{enter}');
+
+    cy.contains('#submit', 'Submit').click();
+
+    cy.get('.modal-content')
+      .should('contain.text', 'Thanks for submitting the form');
+
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .and('contain', user.lastName);
+
+    cy.contains('tr', 'Student Email').should('contain.text', user.email);
+
+    cy.contains('tr', 'Gender').should('contain.text', user.gender);
+
+    cy.contains('tr', 'Mobile').should('contain.text', user.mobileNumber);
+
+    cy.contains('tr', 'Date of Birth')
+      .should('contain', '23 October,2003');
+
+    cy.contains('tr', 'Subject').should('contain.text', 'English, Biology');
+
+    cy.contains('tr', 'Hobbies').should('contain.text', user.hobby);
+
+    cy.contains('tr', 'Picture').should('contain.text', '');
+
+    cy.contains('tr', 'Address').should('contain.text', user.address);
+
+    cy.contains('tr', 'State and City')
+      .should('contain', 'Uttar Pradesh Lucknow');
   });
 });
