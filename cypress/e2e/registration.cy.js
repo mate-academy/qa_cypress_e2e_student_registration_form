06a5343d7af360439075bb970b81bce167b44a63
Olena Lapa <olena.lapa@gmail.com>
2023-07-17 23:11:36 +0300

    Solution Registration form

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -9,52 +9,58 @@ describe('Student Registration page', () => {
     });
   });
 
-  it('should register a new student', () => {
-    cy.findByPlaceholder('First Name')
-      .type(user.firstName);
-    cy.findByPlaceholder('Last Name')
-      .type(user.lastName);
-    cy.get('#userEmail')
-      .type(user.email);
-    cy.contains('.custom-control-label', user.gender)
-      .click();
-    cy.findByPlaceholder('Mobile Number')
-      .type(user.mobileNumber);
-    cy.get('#dateOfBirthInput')
-      .type('{selectall} 2 Dec 2009{enter}');
-    cy.get('.subjects-auto-complete__value-container')
-      .type('en{enter}' + 'phy{enter}');
-    cy.contains('.custom-control-label', user.hobby)
-      .click();
+  it('', () => {
+    it('should register a new student', () => {
+      cy.findByPlaceholder('First Name')
+        .type(user.firstName);
+      cy.findByPlaceholder('Last Name')
+        .type(user.lastName);
+      cy.get('#userEmail')
+        .type(user.email);
+      cy.contains('.custom-control-label', user.gender)
+        .click();
+      cy.findByPlaceholder('Mobile Number')
+        .type(user.mobileNumber);
+      cy.get('#dateOfBirthInput')
+        .type('{selectall} 2 Dec 2009{enter}');
+      cy.get('.subjects-auto-complete__value-container')
+        .type('en{enter}');
+      cy.contains('.custom-control-label', user.hobby)
+        .click();
 
-    cy.findByPlaceholder('Current Address')
-      .type(user.address);
+      cy.findByPlaceholder('Current Address')
+        .type(user.address);
 
-    cy.get('#state')
-      .type('{downarrow}{enter}');
-    cy.get('#city')
-      .type('{downarrow}{enter}');
+      cy.get('#state')
+        .type('{downarrow}{enter}');
+      cy.get('#city')
+        .type('{downarrow}{enter}');
 
-    cy.get('#submit').click();
+      cy.get('#submit').click();
 
-    cy.get('.modal-header')
-      .should('contain.text', 'Thanks for submitting the form');
-    cy.contains('tr', user.firstName).should('be.visible', user.firstName);
-
-    cy.contains('tr', user.lastName).should('be.visible', user.lastName);
-
-    cy.contains('tr', user.email).should('be.visible', user.email);
-
-    cy.contains('tr', user.gender).should('be.visible', user.gender);
-
-    cy.contains('tr', user.mobileNumber)
-      .should('be.visible', user.mobileNumber);
-
-    cy.contains('tr', user.hobby).should('be.visible', user.hobby);
-
-    cy.contains('tr', user.address).should('be.visible', user.address);
-
-    cy.contains('tr', 'State and City')
-      .should('contain', 'Uttar Pradesh Lucknow');
+      cy.get('.modal-content')
+        .should('contain.text', 'Thanks for submitting the form');
+      cy.contains('tr', 'Student Name')
+        .should('contain', user.firstName)
+        .and('contain', user.lastName);
+      cy.contains('tr', 'Student Email')
+        .should('be.visible', user.email);
+      cy.contains('tr', 'Gender')
+        .should('be.visible', user.genders);
+      cy.contains('tr', 'Mobile')
+        .should('be.visible', user.mobileNumber);
+      cy.contains('tr', 'Date of Birth')
+        .should('contain', '02 Dec ,2009');
+      cy.contains('tr', 'Subjects')
+        .should('contain', 'English');
+      cy.contains('tr', 'Hobbies')
+        .should('contain', user.hobby);
+      cy.contains('tr', 'Picture')
+        .should('contain.text', '');
+      cy.contains('tr', 'Address')
+        .should('contain', user.streetAddress);
+      cy.contains('tr', 'State and City')
+        .should('contain', 'Uttar Pradesh Lucknow');
+    });
   });
 });
