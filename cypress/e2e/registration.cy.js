9ad2c8f18880ed296e45b93bfdd493fee8b185e2
Viktoriia Tykhomyrova <viktoriavintonak0@gmail.com>
2023-08-31 12:26:22 +0200

    ./cypress/e2e/registration.cy.js

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -2,10 +2,19 @@
 
 describe('Student Registration page', () => {
   before(() => {
-
+    cy.visit('/register?_k=0id1ly');
   });
 
-  it('', () => {
+  it('should register user', () => {
+    const randomNumber = Math.random().toString().slice(2, 6);
+    const userName = `test_users_${randomNumber}`;
+    const email = `test_${randomNumber}@example.com`;
+    const password = 'password1234';
 
+    cy.get('h1').should('contain.text', 'Sign Up');
+    cy.findByPlaceholder('Username').type(userName);
+    cy.findByPlaceholder('Email').type(email);
+    cy.findByPlaceholder('Password').type(password);
+    cy.get('.btn').click();
   });
 });
