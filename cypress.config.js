320ecf7e78c819dee6dfcc278a661a75591dd879
Artur <masterarturgames@gmail.com>
2023-09-15 16:34:50 +0300

    registration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    baseUrl: 'https://demoqa.com/',
     setupNodeEvents(on, config) {
     }
   }
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,28 @@
 /// <reference types='cypress' />
 
+const fi = require("faker/lib/locales/fi");
+const { generateUser } = require("../support/generateUser");
+
 describe('Student Registration page', () => {
   before(() => {
-
+    cy.visit('/automation-practice-form')
   });
 
-  it('', () => {
+  it('should register new user with valid credentials', () => {
+    const {email, username, password, firstName, lastName, randomNumber} = generateUser()
 
+    cy.get('#firstName').type(firstName)
+    cy.get('#lastName').type(lastName)
+    cy.get('#userEmail').type(email)
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(3) > .custom-control-label').click()
+    cy.get('#userNumber').type(randomNumber)
+    cy.get('.subjects-auto-complete__value-container').type('Art{enter}')
+    cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1)').click()
+    cy.get('#currentAddress').type('tut ya jivu')
+    cy.get('#state').type('Hary{enter}');
+    cy.get('#city').type('Ka{enter}')
+    cy.get('#submit').click()
+    cy.get('.modal-content').should('exist')
+    cy.get('.modal-header').should('contain.text', 'Thanks for submitting the form')
   });
 });
