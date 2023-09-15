e0220fa9effafb224645b1a383df46f7fc067177
Svitlana Zai <svetkaqq1991@gmail.com>
2023-09-15 18:28:00 +0300

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -9,6 +9,9 @@ describe('Student Registration page', () => {
   const email = faker.internet.email();
   const firstName = faker.name.firstName();
   const lastName = faker.name.lastName();
+  const phoneNumber = faker.random
+    .number({ min: 1000000000, max: 9999999999 }).toString();
+  const currentAddress = faker.address.streetAddress();
 
   it('should register new user with valid credentials"', () => {
     cy.viewport(1920, 1080);
@@ -17,7 +20,7 @@ describe('Student Registration page', () => {
     cy.get('[placeholder="Last Name"]').type(lastName);
     cy.get('[placeholder="name@example.com"]').type(email);
     cy.get('[for=gender-radio-3]').click();
-    cy.get('[placeholder="Mobile Number"]').type('1234567890');
+    cy.get('[placeholder="Mobile Number"]').type(phoneNumber);
 
     cy.get('#dateOfBirthInput').click();
     cy.get('.react-datepicker__month-select').select('0');
@@ -26,7 +29,7 @@ describe('Student Registration page', () => {
 
     cy.get('.subjects-auto-complete__value-container').type('com{enter}');
     cy.get('.custom-checkbox [for="hobbies-checkbox-3"]').click();
-    cy.get('[placeholder="Current Address"]').type('Example street');
+    cy.get('[placeholder="Current Address"]').type(currentAddress);
     cy.get('#state').type('Hary{enter}');
     cy.get('#city').type('Ka{enter}');
 
@@ -39,5 +42,7 @@ describe('Student Registration page', () => {
     cy.get('.modal-body').should('contain', firstName);
     cy.get('.modal-body').should('contain', lastName);
     cy.get('.modal-body').should('contain', email);
+    cy.get('.modal-body').should('contain', phoneNumber);
+    cy.get('.modal-body').should('contain', currentAddress);
   });
 });
