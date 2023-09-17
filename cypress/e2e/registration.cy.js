d8f4d11a755f933843ec0e28fddd7e42b3984377
Yelyzaveta <122609036+YelyzavetaFilimonova@users.noreply.github.com>
2023-09-17 21:07:27 +0300

    Update registration.cy.js

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -10,14 +10,17 @@ describe('Student Registration page', () => {
     const firstName = 'Name';
     const lastName = 'Lastname';
     const email = 'test@test.com';
+    const phoneNumber = '1234567890';
+    const address = 'Ukraine';
+    const hobbies = 'Sports';
 
     cy.get('#firstName').type(firstName);
     cy.get('#lastName').type(lastName);
     cy.get('#userEmail').type(email);
     cy.get('[for="gender-radio-2"]').click();
-    cy.get('[placeholder="Mobile Number"]').type('1234567890');
+    cy.get('[placeholder="Mobile Number"]').type('phoneNumber');
     cy.get('[for="hobbies-checkbox-1"]').click();
-    cy.get('#currentAddress').type('Ukraine');
+    cy.get('#currentAddress').type(address);
     cy.get('#state > div > div.css-1hwfws3').type('NCR{enter}');
     cy.get('#city > div > div.css-1hwfws3').type('Delhi{enter}');
     cy.get('#submit').click();
@@ -26,9 +29,9 @@ describe('Student Registration page', () => {
     cy.get('tbody > :nth-child(2) > :nth-child(2)').should('contain', email);
     cy.get('.modal-body').should('contain', firstName + ' ' + lastName);
     cy.get('tbody > :nth-child(3) > :nth-child(2)').should('contain', 'Female');
-    cy.get('.modal-body').should('contain', '1234567890');
-    cy.get('.modal-body').should('contain', 'Sports');
-    cy.get('.modal-body').should('contain', 'Ukraine');
+    cy.get('.modal-body').should('contain', phoneNumber);
+    cy.get('.modal-body').should('contain', hobbies);
+    cy.get('.modal-body').should('contain', address);
     cy.get('.modal-body').should('contain', 'NCR Delhi');
   });
 });
