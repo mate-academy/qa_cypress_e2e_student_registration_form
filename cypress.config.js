9d92547540ae7be249525d104cc917445015bce8
okan3 <okan_ivan@ukr.net>
2023-11-02 18:32:44 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,29 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1320,
+    taskTimeout: 8000,
     setupNodeEvents(on, config) {
-    }
-  }
+      on('task', {
+        generateStudent() {
+          randomIndex = Math.floor(Math.random() * 2);
+          genders = ['Male', 'Female', 'Other'];
+          hobbies = ['Sports', 'Reading', 'Music'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            gender: genders[randomIndex],
+            phoneNumber: faker.phone.phoneNumber('##########'),
+            birthDate: faker.date.past(),
+            hobbie: hobbies[randomIndex],
+            address: faker.address.streetAddress(),
+          };
+        },
+      });
+    },
+  },
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,82 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
+  let user;
 
+  before(() => {
+    cy.visit('https://demoqa.com/automation-practice-form');
+    cy.task('generateStudent').then(generateStudent => {
+      user = generateStudent;
+    });
   });
 
-  it('', () => {
+  it('Register a new student ', () => {
+    cy.get('#firstName')
+      .type(user.firstName);
 
+    cy.get('#lastName')
+      .type(user.lastName);
+
+    cy.get('#userEmail')
+      .type(user.email);
+
+    cy.contains('.custom-control-label', user.gender)
+      .click();
+
+    cy.get('#userNumber')
+      .type(user.phoneNumber);
+
+    cy.get('#dateOfBirthInput')
+      .type('{selectAll} 21 May 2003{enter}');
+
+    cy.get('.subjects-auto-complete__value-container')
+      .type('En{enter}' + 'ci{enter}');
+
+    cy.contains('.custom-control-label', user.hobbie)
+      .click();
+
+    cy.get('#currentAddress')
+      .type(user.address);
+
+    cy.get('#state')
+      .type('{downarrow}{enter}');
+
+    cy.get('#city')
+      .type('{downarrow}{enter}');
+
+    cy.get('.btn-primary')
+      .click();
+
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .should('contain', user.lastName);
+
+    cy.contains('tr', 'Student Email')
+      .should('contain', user.email);
+
+    cy.contains('tr', 'Gender')
+      .should('contain', user.gender);
+
+    cy.contains('tr', 'Mobile')
+      .should('contain', user.phoneNumber);
+
+    cy.contains('tr', 'Date of Birth')
+      .should('contain', '21 May,2003');
+
+    cy.contains('tr', 'Subjects')
+      .should('contain', 'English, Computer Science');
+
+    cy.contains('tr', 'Hobbies')
+      .should('contain', user.hobbie);
+
+    cy.contains('tr', 'Address')
+      .should('contain', user.address);
+
+    cy.contains('tr', 'State and City')
+      .should('contain', 'Uttar Pradesh')
+      .should('contain', 'Lucknow');
+
+    cy.get('#closeLargeModal')
+      .click();
   });
 });
