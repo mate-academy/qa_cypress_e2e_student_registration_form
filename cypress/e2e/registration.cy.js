a4c86b91f896946e01badd962144ea1d7dc02f40
Alex <horonhozov@gmail.com>
2023-11-02 17:23:18 +0200

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,70 @@
 /// <reference types='cypress' />
+const { generateRandomUser } = require('../support/generateUser');
 
 describe('Student Registration page', () => {
   before(() => {
-
+   cy.visit('https://demoqa.com/automation-practice-form');
+   cy.viewport(1920, 1080);
   });
 
-  it('', () => {
+  it('Should fill out all fields except "picture" with random data', () => {
+   const randomData = generateRandomUser();
 
+   cy.get('#firstName')
+    .type(randomData.firstName);
+   cy.get('#lastName')
+    .type(randomData.lastName);
+   cy.get('#userEmail')
+    .type(randomData.email);
+   cy.get('[for="gender-radio-1"]')
+    .click();
+   cy.get('#userNumber')
+    .type(randomData.phone);
+   cy.get('.react-datepicker__input-container [id="dateOfBirthInput"]')
+    .click();
+   cy.get('.react-datepicker__month-select')
+    .select('December');
+   cy.get('.react-datepicker__year-select')
+    .select('1998');
+   cy.get('.react-datepicker__day--024')
+    .click();
+   cy.get('[id="subjectsContainer"]')
+    .type('History' + `{Enter}`);
+   cy.get('[for="hobbies-checkbox-1"]')
+    .click();
+   cy.get('#currentAddress')
+    .type(randomData.address);
+   cy.get('[id="state"]')
+    .click();
+   cy.get('#react-select-3-option-0')
+    .click();
+   cy.get('[id="city"]')
+    .click();
+   cy.get('#react-select-4-option-0')
+    .click();
+   cy.get('[id="submit"]')
+    .click();
+   cy.contains('.modal-content', 'Thanks for submitting the form');
+   
+   cy.get('.modal-body')
+    .should('contain', randomData.firstName);
+   cy.get('.modal-body')
+    .should('contain', randomData.lastName);
+   cy.get('.modal-body')
+    .should('contain', randomData.email);
+   cy.contains('tr', 'Gender')
+    .should('contain', 'Male');
+   cy.get('.modal-body')
+    .should('contain', randomData.phone);
+   cy.contains('tr', 'Date of Birth')
+    .should('contain', '24 December,1998');
+   cy.contains('tr', 'Subjects')
+    .should('contain', 'History');
+   cy.contains('tr', 'Hobbies')
+    .should('contain', 'Sports');
+   cy.get('.modal-body')
+    .should('contain', randomData.address);
+   cy.contains('tr', 'State and City')
+    .should('contain', 'NCR Delhi');
   });
 });
