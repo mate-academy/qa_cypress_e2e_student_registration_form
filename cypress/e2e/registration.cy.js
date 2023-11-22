3a96fbeadfbfbae7d268d29af5637385873346c1
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-11-22 14:04:23 +0200

    solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-force */
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
@@ -9,23 +10,29 @@ describe('Student Registration page', () => {
     cy.get('#firstName').type('Bob');
     cy.get('#lastName').type('Miller');
     cy.get('#userEmail').type('bob.miller@gmail.com');
-    cy.get('#gender-radio-1').check();
+    cy.get('#gender-radio-1').check({ force: true });
     cy.get('#userNumber').type('0678900000');
-    cy.get('#dateOfBirthInput').type('1989-12-12');
-    cy.get('.subjects-auto-complete__value-container').type('Math{enter}');
-    cy.get('#hobbies-checkbox-1').check();
-    cy.get('#currentAddress').type('10 Khreshatik St');
-    cy.get('#state').type('NCR{enter}');
-    cy.get('#city').type('Delhi{enter}');
+    cy.get('#dateOfBirthInput').click();
+    cy.get('.react-datepicker__year-select').select('1991');
+    cy.get('.react-datepicker__month-select').select('December');
+    cy.get('.react-datepicker__day--012').click();
 
-    cy.get('#submit').click();
+    cy.get('[id="subjectsContainer"]').type(`Math{enter}`);
+    cy.get('#hobbies-checkbox-1').check({ force: true });
+    cy.get('#currentAddress').type('10 Khreshatik St');
+    cy.get('#state').click({ force: true });
+    cy.get('#react-select-3-input').type('NCR{enter}', { force: true });
+    cy.get('#city').click({ force: true });
+    cy.get('#react-select-4-input').type('Delhi{enter}', { force: true });
+
+    cy.get('#submit').click({ force: true });
 
     cy.get('.modal-body').should('contain', 'Bob');
     cy.get('.modal-body').should('contain', 'Miller');
     cy.get('.modal-body').should('contain', 'bob.miller@gmail.com');
     cy.get('.modal-body').should('contain', 'Male');
     cy.get('.modal-body').should('contain', '0678900000');
-    cy.get('.modal-body').should('contain', '12 December,1989');
+    cy.get('.modal-body').should('contain', '12 December,1991');
     cy.get('.modal-body').should('contain', '10 Khreshatik St');
   });
 });
