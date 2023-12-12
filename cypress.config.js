323af01bfb05558e36b4859b87b7e3507da15c87
Katarzyna Błaszko-Grądzik <blaszko.gradzik.k@gmail.com>
2023-12-12 16:46:32 +0100

    Solution 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,32 @@
+const { faker } = require('@faker-js/faker');
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportHeight: 1200,
+    viewportWidth: 1200,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          const sex = faker.person.sex();
+          const firstNameG = faker.person.firstName(sex);
+          const lastNameG = faker.person.lastName(sex);
+
+          return {
+            firstName: firstNameG,
+            lastName: lastNameG,
+            email: faker.internet.email({
+              firstName: firstNameG,
+              lastName: lastNameG
+            }),
+            gender: sex,
+            phone: faker.phone.number('##########'),
+            birthDate: faker.date.birthdate().toDateString().slice(3),
+            address: faker.location.streetAddress()
+          };
+        }
+      });
     }
   }
 });
