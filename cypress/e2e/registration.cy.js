aa9819034f4f7bdcf35f17f85bc9f4c5802fe352
Patryk <patryk.kuzma@o2.pl>
2023-12-12 09:02:53 +0100

    added random generation

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,52 +1,66 @@
-/* eslint-disable max-len */
 /// <reference types='cypress' />
+
+// cypress/support/commands.js
+Cypress.Commands.add('generateUser', () => {
+  const randomString = Math.random().toString(36).substring(7);
+  const userEmail = `email${randomString}@example.com`;
+  const userNumber = Math.floor(Math.random() * 10000000000).toString();
+  const subjects = 'Running';
+  const currentAddress = `Adres testowy ${randomString}`;
+
+  return {
+    userEmail,
+    userNumber,
+    subjects,
+    currentAddress
+  };
+});
 
 describe('Student Registration page', () => {
   beforeEach(() => {
+    // Set the viewport size before each test
+    cy.viewport(1800, 1200);
     // Przechodzenie do strony przed każdym testem
     cy.visit('https://demoqa.com/automation-practice-form');
   });
 
   it('Wypełnij formularz bez pola "picture"', () => {
     // Wprowadź dane do formularza
-    const firstName = 'Imie';
-    const lastName = 'Nazwisko';
-    const userEmail = 'email@example.com';
-    const userNumber = '1234567890';
-    const subjects = 'Running';
-    const currentAddress = 'Adres testowy';
+    cy.generateUser().then((randomUserData) => {
+      const {
+        userEmail,
+        userNumber,
+        subjects,
+        currentAddress
+      } = randomUserData;
 
-    cy.get('#firstName').type(firstName);
-    cy.get('#lastName').type(lastName);
-    cy.get('#userEmail').type(userEmail);
-    cy.get('[for="gender-radio-1"]').click(); // Wybierz płci "Mężczyzna"
-    cy.get('#userNumber').type(userNumber);
-    cy.get('#dateOfBirthInput').type('{selectall}06 Dec 2000{enter}');
+      cy.get('#firstName').type('Imie');
+      cy.get('#lastName').type('Nazwisko');
+      cy.get('#userEmail').type(userEmail);
+      cy.get('[for="gender-radio-1"]').click(); // Wybierz płci "Mężczyzna"
+      cy.get('#userNumber').type(userNumber);
+      cy.get('#dateOfBirthInput').type('{selectall}06 Dec 2000{enter}');
 
-    // Zaznacz "Music" w polu przedmiotów
-    cy.get('#subjectsInput').type(subjects);
+      // Zaznacz "Music" w polu przedmiotów
+      cy.get('#subjectsInput').type(subjects);
 
-    // Wybierz zainteresowania
-    cy.get('label[for="hobbies-checkbox-1"]').click();
-    // Pomijamy pole "picture", ponieważ Cypress nie obsługuje ładowania plików w prosty sposób
+      // Wybierz zainteresowania
+      cy.get('label[for="hobbies-checkbox-1"]').click();
+      // Pomijamy pole "picture", ponieważ Cypress nie obsługuje ładowania plików w prosty sposób
 
-    // Wprowadź aktualny adres
-    cy.get('#currentAddress').type(currentAddress);
+      // Wprowadź aktualny adres
+      cy.get('#currentAddress').type(currentAddress);
 
-    // Scroll into view and select the state
-    cy.get('#state').type('{downarrow}{enter}');
-    //
-    cy.get('#city').type('{downarrow}{enter}');
+      // Scroll into view and select the state
+      cy.get('#state').type('{downarrow}{enter}');
 
-    // Scroll into view and select the city
-    // cy.get('#city').scrollIntoView();
-    // cy.get('.css-1fhf3k1-control input').type('Delhi');
-    // cy.get('.css-1fhf3k1-control input').type('{enter}');
+      cy.get('#city').type('{downarrow}{enter}');
 
-    // Kliknij przycisk "Submit"
-    cy.get('#submit').click();
+      // Kliknij przycisk "Submit"
+      cy.get('#submit').click();
 
-    // Assert inputted data in the modal
-    cy.get('.modal-body').should('be.visible');
+      // Assert inputted data in the modal
+      cy.get('.modal-body').should('be.visible');
+    });
   });
 });
