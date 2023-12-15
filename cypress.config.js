caae3a5fe0ac8bf7e34e459c5db0098a664e69ed
mat12722 <mat1272295@wp.pl>
2023-12-15 17:04:40 +0100

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,29 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
     viewportWidth: 1500,
     viewportHeight: 1500,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          const FirstName = faker.person.firstName();
+          const LastName = faker.person.lastName();
+          const Email = faker.internet.email();
+          const Gender = faker.person.sex();
+          const PhoneNumber = faker.phone.imei();
+          const Address = faker.location.city();
+          return {
+            firstName: FirstName,
+            lastName: LastName,
+            email: Email,
+            gender: Gender.charAt(0).toUpperCase() + Gender.slice(1),
+            phoneNumber: PhoneNumber.replaceAll('-', '').slice(0, 10),
+            address: Address
+          };
+        }
+      });
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -6,102 +6,99 @@ describe('Student Registration page', () => {
   });
 
   it('', () => {
-    cy.get('[id="firstName"]')
-      .type('Tom');
+    cy.task('generateUser').then((user) => {
+      cy.get('#firstName')
+        .type(user.firstName);
 
-    cy.get('[id="lastName"]')
-      .type('Smith');
+      cy.get('#lastName')
+        .type(user.lastName);
 
-    cy.get('[id="userEmail"]')
-      .type('tomsmith@example.com');
+      cy.get('[id="userEmail"]')
+        .type(user.email);
 
-    cy.get('#genterWrapper')
-      .contains('Male')
-      .click();
+      cy.get('#genterWrapper')
+        .contains(user.gender)
+        .click();
 
-    cy.get('[id="userNumber"]')
-      .type('1234567890');
+      cy.get('[id="userNumber"]')
+        .type(user.phoneNumber);
 
-    cy.get('[id="dateOfBirthInput"]')
-      .click();
+      cy.get('[id="dateOfBirthInput"]')
+        .click();
 
-    cy.get('.react-datepicker__year-select')
-      .select('2023');
+      cy.get('.react-datepicker__year-select')
+        .select('2023');
 
-    cy.get('.react-datepicker__month-select')
-      .select('December');
+      cy.get('.react-datepicker__month-select')
+        .select('December');
 
-    cy.get('.react-datepicker__day--016')
-      .click();
+      cy.get('.react-datepicker__day--016')
+        .click();
 
-    cy.get('#subjectsInput')
-      .type('Arts{enter}');
+      cy.get('#subjectsInput')
+        .type(`Arts{enter}`);
 
-    cy.get('[for="hobbies-checkbox-1"]')
-      .click();
+      cy.get('[for="hobbies-checkbox-1"]')
+        .click();
 
-    cy.get('#currentAddress')
-      .type('currentAddress');
+      cy.get('#currentAddress')
+        .type(user.address);
 
-    cy.get('#stateCity-wrapper')
-      .contains('Select State')
-      .type('Rajasthan{enter}');
+      cy.get('#stateCity-wrapper')
+        .contains('Select State')
+        .type('Rajasthan{enter}');
 
-    cy.get('#stateCity-wrapper')
-      .contains('Select City')
-      .type('Jaiselmer{enter}');
+      cy.get('#stateCity-wrapper')
+        .contains('Select City')
+        .type('Jaiselmer{enter}');
 
-    cy.get('#submit')
-      .click();
+      cy.get('#submit')
+        .click();
 
-    cy.get('.modal-body')
-      .contains('Student Name')
-      .next('td')
-      .should('have.text', 'Tom Smith');
+      cy.get('.modal-body')
+        .contains('Student Name')
+        .next('td')
+        .should('have.text', user.firstName + ' ' + user.lastName);
 
-    cy.get('.modal-body')
-      .contains('Student Email')
-      .next('td')
-      .should('have.text', 'tomsmith@example.com');
+      cy.get('.modal-body')
+        .contains('Student Email')
+        .next('td')
+        .should('have.text', user.email);
 
-    cy.get('.modal-body')
-      .contains('Student Email')
-      .next('td')
-      .should('have.text', 'tomsmith@example.com');
+      cy.get('.modal-body')
+        .contains('Gender')
+        .next('td')
+        .should('have.text', user.gender);
 
-    cy.get('.modal-body')
-      .contains('Gender')
-      .next('td')
-      .should('have.text', 'Male');
+      cy.get('.modal-body')
+        .contains('Mobile')
+        .next('td')
+        .should('have.text', user.phoneNumber);
 
-    cy.get('.modal-body')
-      .contains('Mobile')
-      .next('td')
-      .should('have.text', '1234567890');
+      cy.get('.modal-body')
+        .contains('Date of Birth')
+        .next('td')
+        .should('have.text', '16 December,2023');
 
-    cy.get('.modal-body')
-      .contains('Date of Birth')
-      .next('td')
-      .should('have.text', '16 December,2023');
+      cy.get('.modal-body')
+        .contains('Subjects')
+        .next('td')
+        .should('have.text', 'Arts');
 
-    cy.get('.modal-body')
-      .contains('Subjects')
-      .next('td')
-      .should('have.text', 'Arts');
+      cy.get('.modal-body')
+        .contains('Hobbies')
+        .next('td')
+        .should('have.text', 'Sports');
 
-    cy.get('.modal-body')
-      .contains('Hobbies')
-      .next('td')
-      .should('have.text', 'Sports');
+      cy.get('.modal-body')
+        .contains('Address')
+        .next('td')
+        .should('have.text', user.address);
 
-    cy.get('.modal-body')
-      .contains('Address')
-      .next('td')
-      .should('have.text', 'currentAddress');
-
-    cy.get('.modal-body')
-      .contains('State and City')
-      .next('td')
-      .should('have.text', 'Rajasthan Jaiselmer');
+      cy.get('.modal-body')
+        .contains('State and City')
+        .next('td')
+        .should('have.text', 'Rajasthan Jaiselmer');
+    });
   });
 });
