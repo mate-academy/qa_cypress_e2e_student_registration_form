80232060b649da311599ce827f2c5e9839c54ef6
Maciej Gajda <ma.gajda@gmail.com>
2023-12-27 15:56:29 +0100

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
@@ -6,7 +7,23 @@ module.exports = defineConfig({
     viewportHeight: 800,
     viewportWidth: 600,
     setupNodeEvents(on, config) {
-    // implement node event listeners here
+      on('task', {
+        // generator usera - faker
+        generateUser() {
+          const gender = faker.person.sex();
+          const firstName = faker.person.firstName(gender);
+          const lastName = faker.person.lastName(gender);
+          return {
+            firstName,
+            lastName,
+            email: faker.internet.email({ firstName, lastName }),
+            gender,
+            phoneNumber: faker.phone.number('##########')
+            //  birthDate: faker.date.birthdate().toDateString().slice(4),
+          };
+        }
+      });
+      // implement node event listeners here
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,25 +1,34 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
+  let user; // tworzymy zmienną user dla generatora
+
   before(() => {
     cy.visit('/automation-practice-form');
+    cy.task('generateUser').then((generatedUser) => {
+      // odwołujemy się do generatora w config.js
+      user = generatedUser;
+    });
   });
 
   it('Student Registration Form - autofilling', () => {
     // asercja poprawności miejsca
     cy.get('.main-header').contains('Practice Form').should('be.visible');
     // wypełnienie formularza
-    cy.get('[placeholder="First Name"]')
-      .type('Donald');
-    cy.get('[placeholder="Last Name"]')
-      .type('Duck');
-    cy.get('[placeholder="name@example.com"]')
-      .type('donald@gmail.com');
-    cy.get('#genterWrapper').contains('Male').click();
-    cy.get('[placeholder="Mobile Number"]')
-      .type('48666666666');
-    cy.get('#dateOfBirthInput')
-      .type('{selectAll}' + '13 June 1945' + '{enter}');
+    cy.get('#firstName').type(user.firstName);
+    cy.get('#lastName').type(user.lastName);
+    cy.get('#userEmail').type(user.email);
+    cy.get('#genterWrapper')
+      .contains(user.gender, { matchCase: false })
+      .click();
+    cy.get('#userNumber').type(user.phoneNumber);
+    // cy.get('#dateOfBirthInput').type(
+    //   '{selectAll}' + user.birthDate + '{enter}'
+    // );
+    cy.get('#dateOfBirthInput').click();
+    cy.get('.react-datepicker__year-select').select('1989');
+    cy.get('.react-datepicker__month-select').select('August');
+    cy.get('.react-datepicker__day--015').click();
     cy.get('#subjectsInput').type('ma{enter}');
     cy.get('#hobbiesWrapper').contains('Sport').click();
     cy.findByPlaceholder('Current Address', 'textarea');
@@ -29,25 +38,39 @@ describe('Student Registration page', () => {
     cy.get('#stateCity-wrapper').contains('Select City').type('De{enter}');
     cy.get('button').contains('Submit').click();
     // asercja danych w popup
-    cy.get('tbody > :nth-child(1) > :nth-child(2)')
-      .should('contain', 'Donald Duck');
-    cy.get('tbody > :nth-child(2) > :nth-child(2)')
-      .should('contain', 'donald@gmail.com');
-    cy.get('tbody > :nth-child(3) > :nth-child(2)')
-      .should('contain', 'Male');
-    cy.get('tbody > :nth-child(4) > :nth-child(2)')
-      .should('contain', '4866666666');
-    cy.get('tbody > :nth-child(5) > :nth-child(2)')
-      .should('contain', '13 June,1945');
-    cy.get('tbody > :nth-child(6) > :nth-child(2)')
+    cy.get('.modal-body')
+      .contains('Student Name')
+      .parent()
+      .should('contain', user.firstName, user.lastName);
+    cy.get('.modal-body')
+      .contains('Student Email')
+      .parent()
+      .should('contain', user.email);
+    cy.get('.modal-body')
+      .contains('Gender')
+      .parent()
+      .contains(user.gender, { matchCase: false });
+    cy.get('.modal-body')
+      .contains('Mobile')
+      .parent()
+      .should('contain', user.phoneNumber);
+    cy.get('.modal-body')
+      .contains('Date of Birth')
+      .parent()
+      .should('contain', '15 August,1989');
+
+    cy.get('.modal-body')
+      .contains('Subjects')
+      .parent()
       .should('contain', 'Maths');
-    cy.get('tbody > :nth-child(7) > :nth-child(2)')
+    cy.get('.modal-body')
+      .contains('Hobbies')
+      .parent()
       .should('contain', 'Sports');
-    cy.get('tbody > :nth-child(8) > :nth-child(2)')
-      .should('contain', '');
-    cy.get('tbody > :nth-child(9) > :nth-child(2)')
-      .should('contain', '');
-    cy.get('tbody > :nth-child(10) > :nth-child(2)')
+    cy.get('.modal-body').contains('Address').parent().should('contain', '');
+    cy.get('.modal-body')
+      .contains('State and City')
+      .parent()
       .should('contain', 'NCR Delhi');
   });
 });
