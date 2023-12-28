e4be1b80a5e71ade52ea49c0587ba7d6daaeb88a
Artur Broichenko <orturko77@gmail.com>
2023-12-28 14:32:51 +0000

    Im freaking done

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,26 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          const randomIndex = Math.floor(Math.random() * 2);
+          const genders = ['Male', 'Female', 'Other'];
+          const hobbies = ['Sports', 'Reading', 'Music'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            genders: genders[randomIndex],
+            phoneNumber: faker.phone.phoneNumber('##########'),
+            hobbies: hobbies[randomIndex],
+            address: faker.address.streetAddress()
+          };
+        }
+      });
     }
   }
 });
