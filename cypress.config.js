ebb60a9c4687916305e70c030003fa17c440efb2
Maksym Tarasov <tarasovmv90@gmail.com>
2023-12-28 20:30:56 +0200

    student_registration_form

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,28 @@
 const { defineConfig } = require('cypress');
-
+const faker = require('faker');
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1200,
+    viewportWidth: 1800,
+
     setupNodeEvents(on, config) {
+     on('task', {
+      generateUser() {
+        randomIndex = Math.floor(Math.random() * 2);
+        gender = ['Male', 'Female', 'Other'];
+        hobby = ['Sports', 'Reading', 'Music'];
+        return {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          email: faker.internet.email(),
+          gender: gender[randomIndex],
+          phone: faker.phone.phoneNumber('093#######'),
+          hobby: hobby[randomIndex],
+          street: faker.address.streetAddress(),
+
+        }
+      }
+     })                        
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,36 @@
 /// <reference types='cypress' />
 
+const { GeneratedIdentifierFlags } = require("typescript");
+let user;
+
 describe('Student Registration page', () => {
   before(() => {
+  cy.visit('https://demoqa.com/automation-practice-form');
+  cy.task('generateUser').then(generateUser => {
+    user = generateUser;
+    });
 
   });
 
-  it('', () => {
+  it('should register user with all needed credentials', () => {
+  cy.findByPlaceholder('First Name').type(user.firstName);
+  cy.findByPlaceholder('Last Name').type(user.lastName);
+  cy.findByPlaceholder('name@example.com').type(user.email);
+  cy.contains('.custom-control-label', user.gender).click();
+  cy.findByPlaceholder('Mobile Number').type(user.phone);
+  cy.get('#dateOfBirthInput').type('{selectAll}15 Jul 1990{enter}');
+  cy.get('#subjectsContainer').type('A{enter}');
+  cy.contains('.custom-control-label', user.hobby).click();
+  cy.get('#currentAddress').click().type(user.street);
+  cy.get('#state > .css-yk16xz-control > .css-1hwfws3').type('N{enter}');
+  cy.get('#city > .css-yk16xz-control > .css-1hwfws3').type('M{enter}');
+  cy.get('td').should('contain.text', user.firstName);
+  cy.get('td').should('contain.text', user.lastName);
+  cy.get('td').should('contain.text', user.email);
+  cy.get('td').should('contain.text', user.gender);
+  cy.get('td').should('contain.text', user.phone);
+  cy.get('td').should('contain.text', user.street);
+
 
   });
 });
