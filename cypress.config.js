f191382f4c2689e82d7c3f569aab7e35afba75de
drabykDi <drabik.diana1104@gmail.com>
2023-12-28 20:29:47 +0200

    Test for success registration

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,27 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1100,
+    viewportWidth: 1400,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          const randomIndex = Math.floor(Math.random() * 2);
+          const gender = ['Male', 'Other'];
+          const hobbies = ['Sport', 'Reading', 'Music'];
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            genders: gender[randomIndex],
+            phone: faker.phone.phoneNumber('##########'),
+            hobbies: hobbies[randomIndex]
+          };
+        }
+      });
     }
   }
 });
