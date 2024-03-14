0df7d02f141f74895d13b3fcb96e2c946bbd80c5
Alina <aliska.nofear@gmail.com>
2024-03-14 13:42:30 +0200

    Add changes in config and generate files

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,27 +1,29 @@
 /// <reference types='cypress' />
 
-const generateFunc = require('../support/generate');
+const { generateDate } = require('../support/generate');
 
 describe('Student Registration page', () => {
+  let user;
+
   before(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
     cy.visit('/automation-practice-form');
   });
 
   it('should allow register student', () => {
-    const user = generateFunc.generateUser();
-    const gender = generateFunc.generateGender();
-    const hobbie = generateFunc.generateHobbies();
-    const date = generateFunc.generateDate();
+    const date = generateDate();
 
     // Fill all fields in forms except "picture"
     cy.getAndTypeText('#firstName', user.firstName);
     cy.getAndTypeText('#lastName', user.lastName);
     cy.getAndTypeText('#userEmail', user.email);
-    cy.contains('.custom-control-label', gender).click();
+    cy.contains('.custom-control-label', user.gender).click();
     cy.getAndTypeText('#userNumber', user.mobileNumber);
     cy.getAndTypeText('.react-datepicker-wrapper', `{selectall}${date}{enter}`);
     cy.getAndTypeText('#subjectsContainer', 'a{enter}');
-    cy.contains('.custom-control-label', hobbie).click();
+    cy.contains('.custom-control-label', user.hobbie).click();
     cy.getAndTypeText('#currentAddress', user.address);
     cy.getAndTypeText('#state', 'h{enter}');
     cy.getAndTypeText('#city', 'a{enter}');
@@ -32,11 +34,11 @@ describe('Student Registration page', () => {
       .should('contain.text', 'Thanks for submitting the form');
     cy.chechTableDataByValue(`${user.firstName} ${user.lastName}`);
     cy.chechTableDataByValue(user.email);
-    cy.chechTableDataByValue(gender);
+    cy.chechTableDataByValue(user.gender);
     cy.chechTableDataByValue(user.mobileNumber);
     cy.chechBirthDate();
     cy.chechTableDataBySelector('.css-12jo7m5');
-    cy.chechTableDataByValue(hobbie);
+    cy.chechTableDataByValue(user.hobbie);
     cy.chechTableDataByValue(user.address);
     cy.chechTableDataBySelector('#state');
     cy.chechTableDataBySelector('#city');
--- cypress/support/generate.js
+++ cypress/support/generate.js
@@ -1,29 +1,3 @@
-const { faker } = require('@faker-js/faker');
-
-function generateUser() {
-  const firstName = faker.person.firstName();
-  const lastName = faker.person.lastName();
-  const email = faker.internet.email();
-  const mobileNumber = Math.random().toString().slice(2, 10);
-  const address = faker.location.city();
-
-  return { email, lastName, firstName, mobileNumber, address };
-}
-
-function generateGender() {
-  const genderArr = ['Male', 'Female', 'Other'];
-  const gender = genderArr[(Math.floor(Math.random() * genderArr.length))];
-
-  return gender;
-}
-
-function generateHobbies() {
-  const hobbiesArr = ['Sports', 'Reading', 'Music'];
-  const hobbie = hobbiesArr[(Math.floor(Math.random() * hobbiesArr.length))];
-
-  return hobbie;
-}
-
 function generateDate() {
   const randomYear = Math.floor(Math.random() * (2023 - 1950 + 1)) + 1950;
   const randomMonth = String(Math.floor(Math.random() * 12) + 1)
@@ -53,4 +27,4 @@ function convertShortMonthToFull(shortMonth) {
   return month;
 }
 
-module.exports = { generateUser, generateGender, generateHobbies, generateDate, convertShortMonthToFull };
+module.exports = { generateDate, convertShortMonthToFull };
