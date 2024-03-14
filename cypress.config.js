92b72baa7feb1e72b01edcc39e9fbe919819bea4
Tetiana Maksiuk <tetianamaksiuk@gmail.com>
2024-03-14 20:32:28 +0200

    add some fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/',
+    pageLoadTimeout: 90000,
     setupNodeEvents(on, config) {
     }
   }
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -2,6 +2,22 @@
 const { generateUser} = require('../support/generate');
 const user = generateUser();
 const successMessage = 'Thanks for submitting the form';
+const birthDate = user.birth;
+const months = {
+  Jan: 'January',
+  Feb: 'February',
+  Mar: 'March',
+  Apr: 'April',
+  May: 'May',
+  Jun: 'June',
+  Jul: 'July',
+  Aug: 'August',
+  Sep: 'September',
+  Oct: 'October',
+  Nov: 'November',
+  Dec: 'December'
+};
+const formattedDate = `${birthDate.day} ${months[birthDate.month]},${birthDate.year}`;
 
 describe('Student Registration page', () => {
   before(() => {
@@ -22,5 +38,14 @@ describe('Student Registration page', () => {
     cy.findById('city').type(`${user.city}{enter}`);
     cy.findById('submit').click();
     cy.findById('example-modal-sizes-title-lg').contains(successMessage).should('be.visible');
+    cy.contains('Student Name').next('td').should('have.text', `${user.firstName} ${user.lastName}`);
+    cy.contains('Student Email').next('td').should('have.text', `${user.email}`);
+    cy.contains('td', `${user.gender}`, { timeout: 10000 }).should('exist');
+    cy.contains('td', `${user.mobile}`).should('exist');
+    cy.contains('td', `${formattedDate}`).should('exist');
+    cy.contains('td', `${user.subjectRandom}`).should('exist');
+    cy.contains('td', `${user.hobby}`).should('exist');
+    cy.contains('td', `${user.address}`).should('exist');
+    cy.contains('td', `${user.state} ${user.city}`).should('exist');
+    });
   });
-});
