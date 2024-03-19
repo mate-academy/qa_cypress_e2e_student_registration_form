c117562e2d97f4afda7431e7715ef2a3d4fdaf02
Anton Huskov <gusiara301198@gmail.com>
2024-03-19 22:39:37 +0200

    solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,45 @@
 /// <reference types='cypress' />
-
-describe('Student Registration page', () => {
+describe("Student Registration page", () => {
+  let user;
   before(() => {
-
+    cy.visit("/");
+    cy.task("generateUser").then((generatedUser) => {
+      user = generatedUser;
+    });
   });
 
-  it('', () => {
+  it("should provide the ability to register", () => {
+    cy.findById("firstName").type(user.firstName);
+    cy.findById("lastName").type(user.lastName);
+    cy.findById("userEmail").type(user.email);
+    cy.get(".custom-control-label").contains(user.gender).click();
+    cy.findById("userNumber").type(user.phoneNumber);
 
+    cy.findById("dateOfBirthInput").click();
+    cy.selectMonth(user.birth.month);
+    cy.selectYear(user.birth.year);
+    cy.selectDay(user.birth.day);
+
+    cy.findById("subjectsContainer").type(`E{enter}`);
+    cy.get(".custom-control-label").contains(user.hobbies).click();
+    cy.findById("currentAddress").type(user.adress);
+    cy.findById("state").type("{downarrow}{enter}");
+    cy.findById("city").type("{downarrow}{enter}");
+    cy.findById("submit").click();
+
+    cy.findByTag("Student Name")
+      .should("contain", user.firstName)
+      .and("contain", user.lastName);
+    cy.findByTag("Student Email").should("contain", user.email);
+    cy.findByTag("Gender").should("contain", user.gender);
+    cy.findByTag("Mobile").should("contain", user.phoneNumber);
+    cy.findByTag("Date of Birth").should("contain", user.birthDate);
+    cy.findByTag("Subjects").should("contain", "English");
+    cy.findByTag("Hobbies").should("contain", user.hobbies);
+    cy.findByTag("Address").should("contain", user.adress);
+    cy.findByTag("Student Name").should("contain", user.firstName);
+    cy.findByTag("State and City")
+      .should("contain", "Uttar Pradesh")
+      .and("contain", "Lucknow");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("findById", (name) => {
+  cy.get(`#${name}`);
+});
+
+Cypress.Commands.add("findByTag", (name) => {
+  cy.contains("tr", `${name}`);
+});
+
+Cypress.Commands.add("selectMonth", (month) => {
+  cy.get(".react-datepicker__month-select").select(month);
+});
+
+Cypress.Commands.add("selectYear", (year) => {
+  cy.get(".react-datepicker__year-select").select(year);
+});
+
+Cypress.Commands.add("selectDay", (day) => {
+  cy.get(".react-datepicker__day")
+    .not(".react-datepicker__day--outside-month")
+    .contains(day)
+    .click();
+});
