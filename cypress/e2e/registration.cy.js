7e85a99ca446b004a604e2516e62c98fc242d3da
Antonio S <soi4.toxa@ukr.net>
2024-04-06 15:23:58 +0300

    Added fill-form test

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,76 @@
 /// <reference types='cypress' />
 
+function getDate() {
+  const date = new Date();
+
+  const day = date.getDate();
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'];
+  const monthIndex = date.getMonth();
+  const month = monthNames[monthIndex];
+
+  const year = date.getFullYear();
+
+  return `${day} ${month},${year}`;
+}
+
 describe('Student Registration page', () => {
-  before(() => {
+  it('should open modal window with same data upon valid data', () => {
+    cy.visit('https://demoqa.com/automation-practice-form');
 
-  });
+    const today = getDate();
+    const data = {
+      firstName: 'FirstName',
+      lastName: 'LastName',
+      userEmail: 'FirstName@email.test',
+      userNumber: '0123456789',
+      address: 'Peremohy Street, 4',
+      state: 'NCR',
+      city: 'Noida',
+      gender: 'Female',
+      subjects: ['English', 'Maths'],
+      hobbies: ['Sports', 'Reading']
+    };
 
-  it('', () => {
+    cy.fillForm(data);
+    cy.contains('button', 'Submit').click();
 
+    cy.contains('td', 'Student Name').next('td')
+      .should('contain.text', data.firstName + ' ' + data.lastName);
+
+    cy.contains('td', 'Student Email').next('td')
+      .should('contain.text', data.userEmail);
+
+    cy.contains('td', 'Gender').next('td')
+      .should('contain.text', data.gender);
+
+    cy.contains('td', 'Mobile').next('td')
+      .should('contain.text', data.userNumber);
+
+    cy.contains('td', 'Date of Birth').next('td')
+      .should('contain.text', today);
+
+    cy.contains('td', 'Subjects').next('td')
+      .should('contain.text', data.subjects.join(', '));
+
+    cy.contains('td', 'Hobbies').next('td')
+      .should('contain.text', data.hobbies.join(', '));
+
+    cy.contains('td', 'Address').next('td')
+      .should('contain.text', data.address);
+
+    cy.contains('td', 'State and City').next('td')
+      .should('contain.text', data.state + ' ' + data.city);
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,42 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getById', (id) => {
+  cy.get(`#${id}`);
+});
+
+Cypress.Commands.add('fillForm', ({
+  firstName,
+  lastName,
+  userEmail,
+  userNumber,
+  address,
+  state,
+  city,
+  gender,
+  subjects,
+  hobbies
+}) => {
+  cy.getById('firstName').type(firstName);
+  cy.getById('lastName').type(lastName);
+  cy.getById('userEmail').type(userEmail);
+  cy.contains('label', gender).click();
+  cy.getById('userNumber').type(userNumber);
+
+  subjects.forEach((subject) => {
+    cy.getById('subjectsInput').type(subject + `{Enter}`);
+  });
+
+  hobbies.forEach((hobby) => {
+    cy.contains('label', hobby).click();
+  });
+
+  cy.getById('currentAddress').type(address);
+
+  cy.getById('state').click();
+  cy.contains('div', state).click();
+
+  cy.getById('city').click();
+  cy.contains('div', city).click();
+});
