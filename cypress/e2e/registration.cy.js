db8dcf30d17af5a8282d0aa305b21a4cdf60c9f4
Anastasiya145 <a.ivanova-external@powernext.com>
2024-04-11 15:58:04 +0200

    the task is done

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,22 @@
+import generateUser from '../support/generateNewUser';
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
+  let user;
 
+  before(() => {
+    cy.visit('https://demoqa.com/automation-practice-form');
+    user = generateUser();
   });
 
-  it('', () => {
+  it('should fill the form and register a new user', () => {
+    cy.fillForm(user);
+    cy.get('#submit').click();
 
+    cy.get('#modal-header')
+      .should('contain.text', 'Thanks for submitting the form');
+    cy.checkValuesInTable(user);
+    cy.get('button[id="closeLargeModal"]')
+      .should('contain.text', 'Close').click();
   });
 });
