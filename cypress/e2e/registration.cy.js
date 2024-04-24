fac24499c11ddb11b851afeece9791d16bf2c261
Ekaterina Popova <ekaterinapopova1382@gmail.com>
2024-04-24 13:08:14 +0200

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,38 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
+  let user;
 
+  before(() => {
+    cy.visit(`https://demoqa.com/automation-practice-form`);
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
-
+  it('should register a new student', () => {
+    cy.findByPlaceholder('First Name').type(user.firstName);
+    cy.findByPlaceholder('Last Name').type(user.lastName);
+    cy.get('#userEmail').type(user.email);
+    cy.contains('.custom-control-label', user.gender).click();
+    cy.findByPlaceholder('Mobile Number').type(user.mobileNumber);
+    cy.get('#dateOfBirthInput')
+      .click();
+    cy.get('#dateOfBirthInput').type('{selectAll}14 Aug 1996{enter}');
+    cy.get('#subjectsContainer').type(user.subject + '{enter}');
+    cy.get('.custom-control').contains(user.hobby).click();
+    cy.findByPlaceholder('Current Address').type(user.address);
+    cy.get('#state').type('N{downArrow}{enter}');
+    cy.get('#city').type('D{downArrow}{enter}');
+    cy.contains('#submit', 'Submit').click();
+    cy.contains('tr', 'Student Name').should('contain', user.firstName)
+      .and('contain', user.lastName);
+    cy.contains('tr', 'Student Email').should('contain', user.email);
+    cy.contains('tr', 'Gender').should('contain', user.gender);
+    cy.contains('tr', 'Mobile').should('contain', user.mobileNumber);
+    cy.contains('tr', 'Date of Birth').should('contain', '14 August,1996');
+    cy.contains('tr', 'Hobbies').should('contain', user.hobby);
+    cy.contains('tr', 'Address').should('contain', user.address);
+    cy.contains('tr', 'State and City').should('contain', 'NCR Delhi');
   });
 });
