67537ef68bde61f8e261a3b3d874734f962aea84
Pavlo Roiev <157385125+SilverF1nger@users.noreply.github.com>
2024-05-10 11:53:48 +0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    retries: {
-      runMode: 2
-    }
     setupNodeEvents(on, config) {
     }
   }
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -13,17 +13,25 @@ describe('Student Registration page', () => {
       cy.get('input[placeholder*="Last Name"]').as('lastNameInput').should('exist').type(lastName);                
       cy.get('input[placeholder*="name@example.com"]').as('emailInput').should('exist').type(email);                
       cy.get('.custom-control-label').contains(gender).click();                
-      cy.get('input[placeholder*="Mobile Number"]').as('mobileInput').should('exist').type(mobile);                
-      cy.get('.subjects-auto-complete__value-container').type(subject + '{enter}').should('exist');                
-      cy.get('.custom-control-label').contains(hobbies).click();               
+
+      cy.get('input[placeholder*="Mobile Number"]').as('mobileInput').should('exist').type(mobile);  
+
+      cy.get('.subjects-auto-complete__value-container').type(subject + '{enter}').should('exist');  
+
+      cy.get('.custom-control-label').contains(hobbies).click(); 
+
       cy.get('#dateOfBirthInput').click().should('exist');                
       cy.get('.react-datepicker__month-select').select('March').should('exist');
       cy.get('.react-datepicker__year-select').select('1995').should('exist');
       cy.get(':nth-child(5) > .react-datepicker__day--028').click();
-      cy.get('#currentAddress').as('addressInput').type(address).should('exist');        
+
+      cy.get('#currentAddress').as('addressInput').type(address).should('exist');
+
       cy.get('.css-1wa3eu0-placeholder').contains('Select State').as('stateDropdown').should('exist').type(state + '{enter}');                        
       cy.get('.css-1wa3eu0-placeholder').contains('Select City').as('cityDropdown').should('exist').type(city + '{enter}');
+      
       cy.get('#submit').contains('Submit').should('exist').click();
+
       cy.get('.table-responsive').within(() => {
         cy.contains(`${firstName} ${lastName}`).should('exist');
         cy.contains(email).should('exist');
