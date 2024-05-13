979f258942111acd3dd81f32bd24022a1b1d7102
Tanya <tmfancy@ukr.net>
2024-05-13 18:47:12 +0300

    registration3

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,34 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          const hobbies = ['Sports', 'Reading', 'Music'];
+          const randomIndex = Math.floor(Math.random() * 2);
+          const genders = ['Male', 'Female', 'Other'];
+          return {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            randomGenderIndex: genders[randomIndex],
+            mobileNumber: '1111111111',
+            birth: {
+              year: Math.floor(2000 + Math.random() * 20),
+              month: faker.date.month(),
+              day: Math.ceil(Math.random() * 28)
+            },
+            hobby: hobbies[randomIndex],
+            address: faker.location.streetAddress()
+
+          };
+        }
+      });
     }
   }
 });
