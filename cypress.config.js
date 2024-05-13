09ec5f2822c38c82f8303c31517659c7f7c896b5
Oleksii <aleksey.gudilkin@gmail.com>
2024-05-13 17:28:01 +0300

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,30 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          return {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            randomGenderIndex: Math.floor(Math.random() * 3) + 1,
+            phone: faker.phone.number('##########'),
+            birth: {
+              month: faker.date.month(),
+              year: Math.floor(2000 + Math.random() * 20),
+              day: Math.ceil(Math.random() * 28)
+            },
+            hobbies: Math.ceil(Math.random() * 3),
+            address: faker.location.streetAddress(true)
+          };
+        }
+      });
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,54 @@
+/* eslint-disable max-len */
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
-
+  let user;
+  beforeEach(() => {
+    cy.visit('/');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
+  it('should register an account with all fields filled except the "picture" field', () => {
+    cy.findByPlaceholder('First Name').type(user.firstName);
+    cy.findByPlaceholder('Last Name').type(user.lastName);
+    cy.findByPlaceholder('name@example.com').type(user.email);
+    cy.get(`#gender-radio-2`).click({ force: true });
 
+    cy.findByPlaceholder('Mobile Number').type(user.phone);
+    cy.get('#dateOfBirthInput').click();
+    cy.chooseDate('month-select').select(user.birth.month);
+    cy.chooseDate('year-select').select(`${user.birth.year}`);
+    cy.chooseDate('day').contains(user.birth.day).click();
+
+    cy.get('.subjects-auto-complete__value-container')
+      .type('Ph{enter}' + 'a{downarrow}{enter}');
+    cy.get('#hobbies-checkbox-3').click({ force: true });
+
+    cy.findByPlaceholder('Current Address').type(user.address);
+    cy.get('#state').type('Ut{enter}');
+    cy.get('#city').type('{downarrow}{enter}');
+    cy.get('#submit').click();
+
+    cy.contains('tr', 'Student Name')
+      .should('contain', user.firstName)
+      .and('contain', user.lastName);
+    cy.contains('tr', 'Student Email')
+      .should('contain', user.email);
+    cy.contains('tr', 'Gender')
+      .should('contain', 'Female');
+    cy.contains('tr', 'Mobile')
+      .should('contain', user.phone);
+    cy.contains('tr', 'Date of Birth')
+      .should('contain', `${user.birth.day} ${user.birth.month},${user.birth.year}`);
+    cy.contains('tr', 'Subjects')
+      .should('contain', 'Physics, Accounting');
+    cy.contains('tr', 'Hobbies')
+      .should('contain', 'Music');
+    cy.contains('tr', 'Address')
+      .should('contain', user.address);
+    cy.contains('tr', 'State and City')
+      .should('contain', 'Uttar Pradesh Lucknow');
   });
 });
