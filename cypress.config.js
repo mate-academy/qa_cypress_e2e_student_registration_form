7fcba20367e534b1cde6382e6f295ef3c78ed927
Ihor Sokolenko <igorsokolenko1986@gmail.com>
2024-05-13 20:18:11 +0300

    generatedUser

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,33 @@
+const { faker } = require('@faker-js/faker');
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
+    viewportHeight: 1080,
+    viewportWidth: 1320,
     setupNodeEvents(on, config) {
-    }
-  }
+      on('task', {
+        generateUser() {
+          const hobbies = ['Sports', 'Reading', 'Music'];
+          const randomIndex = Math.floor(Math.random() * 1);
+          const genders = ['Male', 'Female', 'Other'];
+          return {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            randomGenderIndex: genders[randomIndex],
+            mobileNumber: '1111111111',
+            birth: {
+              year: Math.floor(2000 + Math.random() * 20),
+              month: faker.date.month(),
+              day: Math.ceil(Math.random() * 28),
+            },
+            hobby: hobbies[randomIndex],
+            address: faker.location.streetAddress(),
+          };
+        },
+      });
+    },
+  },
 });
