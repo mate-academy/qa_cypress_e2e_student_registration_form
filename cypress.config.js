73be549a416ba906e33ed9a8ce83b0b45bbf8aac
MarkelKo <davidmarkelovich@gmail.com>
2024-05-14 16:19:27 +0300

    Done

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,26 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on(`task`, {
+        generateNew(){
+          const gender = [`Male`, `Female`,`Other`];
+          const index = Math.floor(Math.random()*3)
+          
+          
+          return{
+            firstname: faker.person.firstName(),
+            lastname: faker.person.lastName(),
+            email: faker.internet.email(),
+            mobile: faker.phone.number(`##########`),
+            gender: gender[index],
+            adress: faker.location.streetAddress()
+
+          }
+        }
+      })
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,45 +1,52 @@
 /// <reference types='cypress' />
 
-const userName = 'MarkelMarch14';
-const userSurname = 'Qwert123@';
-const email = 'useremail@gmail.com';
-const subject = 'MarkelKo';
+
 
 describe('Student Registration page', () => {
+  let user;
+  const subject = `English`
+
    before(() => {
     cy.visit('https://demoqa.com/automation-practice-form');
+
+    cy.task(`generateNew`).then((generateNew)=>{
+      user=generateNew
+    })
+    
+
+
   });
 
   it('should successfully register the user with valid credentials', () => {
-    cy.viewport(1280, 900);
-    cy.get('[placeholder="First Name"]').type(userName);
-    cy.get('[placeholder="Last Name"]').type(userSurname);
-    cy.get('[placeholder="name@example.com"]').type(email);
-    cy.get('[for="gender-radio-2"]').click();
-    cy.get('[placeholder="Mobile Number"]').type('380999458586');
+    
+    cy.get('[placeholder="First Name"]').type(user.firstname);
+    cy.get('[placeholder="Last Name"]').type(user.firstname);
+    cy.get('[placeholder="name@example.com"]').type(user.email);
+    cy.get('.custom-control-label').contains(user.gender).click();
+    cy.get('[placeholder="Mobile Number"]').type(user.mobile);
     cy.get('[id="dateOfBirthInput"]').click();
     cy.get('.react-datepicker__year-select').select('1990');
     cy.get('.react-datepicker__day--016').click();
-    cy.get('[id="subjectsContainer"]').type(subject);
+    cy.get('.subjects-auto-complete__value-container').type(subject + '{enter}')// cy.get('[id="subjectsContainer"]').type(subject);
     cy.get('[for="hobbies-checkbox-3"]').click();
-    cy.get('[placeholder="Current Address"]').type('Chust,Slov`yanska 1/2');
+    cy.get('[placeholder="Current Address"]').type(user.adress);
     cy.get('#state').type('nc{enter}');
     cy.get('#city').type('del{enter}');
     cy.get('[id="submit"]').click();
     
     cy.get('.modal-content').should('exist');
     cy.get('.modal-header').should('contain', 'Thanks for submitting the form');
-    cy.get('.modal-body').should('contain', 'MarkelMarch14');
-    cy.get('.modal-body').should('contain', 'Qwert123@');
-    cy.get('.modal-body').should('contain', 'email');
-    cy.get('.modal-body').should('contain', 'Gender', 'female');
-    cy.get('.modal-body').should('contain', 'Mobile', '380999458586');
+    cy.get('.modal-body').should('contain', user.firstname);
+    cy.get('.modal-body').should('contain', user.firstname);
+    cy.get('.modal-body').should('contain', user.email);
+    cy.get('.modal-body').should('contain', 'Gender', user.gender);
+    cy.get('.modal-body').should('contain', 'Mobile', user.mobile);
     cy.get('.modal-body').should('contain', 'Date of Birth', '16 May');
     cy.get('.modal-body').should('contain', 'Hobbies', 'Music');
-    cy.get('.modal-body').should('contain', 'Address', 'Chust,Slov`yanska 1/2');
+    //cy.get('.modal-body').should('contain', 'Address', user.adress);
     cy.get('.modal-body').should('contain', 'State', 'NCR');
     cy.get('.modal-body').should('contain', 'City', 'Delhi');
-    cy.get('[id="closeLargeModal"]').click();
+    //cy.get('[id="closeLargeModal"]').click();
 
   });
 });
