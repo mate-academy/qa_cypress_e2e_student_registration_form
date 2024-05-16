42094a4690d86a53c9a51cc2ca93a2344b1ec443
Yurii Mohylnytskyi <yuriistarlife@gmail.com>
2024-05-16 12:09:25 +0300

    It is ALIVE

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,59 @@
 const { defineConfig } = require('cypress');
-
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/automation-practice-form',
     setupNodeEvents(on, config) {
+      on('task', {
+        generateUser() {
+          function generateRandomEnglishString(length) {
+            const characters =
+              'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+            let randomString = '';
+
+            for (let i = 0; i < length; i++) {
+              const randomIndex = Math.floor(Math.random() * characters.length);
+              randomString += characters[randomIndex];
+            }
+
+            return randomString;
+          }
+
+          function generateRandomEmail() {
+            const firstName = generateRandomEnglishString(5);
+            const lastName = generateRandomEnglishString(10);
+            const domain1 = generateRandomEnglishString(7);
+            const domain2 = generateRandomEnglishString(4);
+            return `${firstName}.${lastName}@${domain1}.${domain2}`;
+          }
+
+          function funRandomGender() {
+            const genders = ['Male', 'Female', 'Other'];
+            const random0to2 = Math.floor(Math.random() * 3);
+            const gender = genders[random0to2];
+            return gender;
+          };
+
+          const testData = {
+            firstName: generateRandomEnglishString(5),
+            lastName: generateRandomEnglishString(10),
+            email: generateRandomEmail(),
+            mobileNumber:
+              Math.floor(Math.random() * (9999999999 - 1000000000 + 1)) +
+              1000000000,
+            birthDay: (Math.floor(Math.random() * 28) + 1).toString(),
+            birthMonth: Math.floor(Math.random() * 12),
+            birthYear: (
+              Math.floor(Math.random() * (2100 - 1900 + 1)) + 1900
+            ).toString(),
+            randomGender: funRandomGender(),
+            randomHobbie1: (Math.floor(Math.random() * 3) + 1).toString(),
+            randomHobbie2: (Math.floor(Math.random() * 3) + 1).toString(),
+            randomHobbie3: (Math.floor(Math.random() * 3) + 1).toString()
+          };
+          return { testData };
+        }
+      });
     }
   }
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,78 +1,79 @@
 /// <reference types='cypress' />
-/// ---
-function generateRandomEnglishString(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-  let randomString = '';
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    randomString += characters[randomIndex];
-  }
-
-  return randomString;
-};
-
-function generateRandomEmail() {
-  const firstName = generateRandomEnglishString(5);
-  const lastName = generateRandomEnglishString(10);
-  const domain1 = generateRandomEnglishString(7);
-  const domain2 = generateRandomEnglishString(4);
-  return `${firstName}.${lastName}@${domain1}.${domain2}`;
-};
-
-const testData = {
-  firstName: generateRandomEnglishString(5),
-  lastName: generateRandomEnglishString(10),
-  email: generateRandomEmail(),
-  mobileNumber: Math.floor(Math.random() * 9999999999) + 999999999,
-  birthDay: (Math.floor(Math.random() * 28) + 1).toString(),
-  birthMonth: Math.floor(Math.random() * 12),
-  birthYear: (Math.floor(Math.random() * (2100 - 1900 + 1)) + 1900).toString(),
-  randomGender: (Math.floor(Math.random() * 3) + 1).toString(),
-  randomHabbit1: (Math.floor(Math.random() * 3) + 1).toString(),
-  randomHabbit2: (Math.floor(Math.random() * 3) + 1).toString(),
-  randomHabbit3: (Math.floor(Math.random() * 3) + 1).toString()
-};
-/// ---
+let user;
 describe('Student Registration page', () => {
   beforeEach(() => {
-    cy.visit('https://demoqa.com/automation-practice-form');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.visit('');
   });
 
-  it('shoud register a new student', () => {
-    cy.get('#firstName').type(testData.firstName);
-    cy.get('#lastName').type(testData.lastName);
-    cy.get('#userEmail').type(testData.email);
+  for (let i = 0; i <= 20; i++) {
+    it('shoud register a new student', () => {
+      const { testData } = user;
+      cy.get('#firstName').type(testData.firstName);
+      cy.get('#lastName').type(testData.lastName);
+      cy.get('#userEmail').type(testData.email);
 
-    cy.get(`[for="gender-radio-${testData.randomGender}"]`).click();
+      cy.get('.custom-control-label').contains(testData.randomGender).click();
 
-    cy.get('#userNumber').type(testData.mobileNumber);
+      cy.get('#userNumber').type(testData.mobileNumber);
 
-    cy.get('#dateOfBirthInput').click();
-    cy.birthDate('month-select').select(testData.birthMonth);
-    cy.birthDate('year-select').select(testData.birthYear);
-    cy.birthDate('month').contains(testData.birthDay).click();
+      cy.get('#dateOfBirthInput').click();
+      cy.birthDate('month-select').select(testData.birthMonth);
+      cy.birthDate('year-select').select(testData.birthYear);
+      cy.birthDate('month').contains(testData.birthDay).click();
 
-    cy.get('.subjects-auto-complete__value-container').type(
-      'En{downarrow}{enter}' + 'En{enter}'
-    );
+      cy.get('.subjects-auto-complete__value-container').type(
+        'En{downarrow}{enter}' + 'En{enter}'
+      );
 
-    cy.hobbies(testData.randomHabbit1).click();
-    cy.hobbies(testData.randomHabbit2).click();
-    cy.hobbies(testData.randomHabbit3).click();
+      cy.hobbies(testData.randomHobbie1).click();
+      cy.hobbies(testData.randomHobbie2).click();
+      cy.hobbies(testData.randomHobbie3).click();
 
-    cy.get('#currentAddress').type(
-      `Name:${testData.firstName} ${testData.lastName};
+      cy.get('#currentAddress').type(
+        `Name:${testData.firstName} ${testData.lastName};
 email:${testData.email}; mobile:${testData.mobileNumber};
 birthDay:${testData.birthDay}; birthMonth:${testData.birthMonth};
 birthYear:${testData.birthYear}; Gender:${testData.randomGender};
-Habbits:${testData.randomHabbit1},${testData.randomHabbit2},${testData.randomHabbit3}`
-    );
+Hobbies:${testData.randomHobbie1},${testData.randomHobbie2},${testData.randomHobbie3}`
+      );
 
-    cy.get('#state').type('{downarrow}{enter}');
-    cy.get('#city').type('{downarrow}{enter}');
+      cy.get('#state').type('{downarrow}{enter}');
+      cy.get('#city').type('{downarrow}{enter}');
 
-    // cy.get('#submit').click();
-  });
+      cy.get('#submit').click();
+
+      cy.contains('tr', 'Student Name').should(
+        'contain',
+        `${testData.firstName} ${testData.lastName}`
+      );
+      cy.contains('tr', 'Gender').should('contain', testData.randomGender);
+      cy.contains('tr', 'Mobile').should('contain', testData.mobileNumber);
+      cy.contains('tr', 'Date of Birth').should('contain', testData.birthYear);
+      cy.contains('tr', 'Subjects').should('contain', 'Computer');
+      cy.contains('tr', 'Picture').should('contain', '');
+      cy.contains('tr', 'Address')
+        .should('contain', `Name:${testData.firstName} ${testData.lastName}`)
+        .and(
+          'contain',
+          `email:${testData.email}; mobile:${testData.mobileNumber}`
+        )
+        .and(
+          'contain',
+          `birthDay:${testData.birthDay}; birthMonth:${testData.birthMonth}`
+        )
+        .and(
+          'contain',
+          `birthYear:${testData.birthYear}; Gender:${testData.randomGender}`
+        )
+        .and(
+          'contain',
+          `Hobbies:${testData.randomHobbie1},${testData.randomHobbie2},${testData.randomHobbie3}`
+        );
+      cy.contains('tr', 'State and City').should('contain', 'Uttar');
+    });
+  }
 });
