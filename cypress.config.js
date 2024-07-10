5ec7c1804c1cb773b61233f74472ead8c3190fa5
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-07-10 19:03:28 +0200

    solved

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    }
-  }
+    baseUrl: "https://demoqa.com/automation-practice-form",
+    pageLoadTimeout: 10000,
+    setupNodeEvents(on, config) {},
+  },
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,50 @@
 /// <reference types='cypress' />
 
-describe('Student Registration page', () => {
-  before(() => {
+import { CITY, HOBBIES, SEX, STATE } from "../support/constant";
 
+describe("Student Registration page", () => {
+  const user = {
+    firstName: "TestName",
+    lastName: "TestSurName",
+    userEmail: "email@mail.com",
+    userSex: SEX.male,
+    userNumber: "0123456789",
+    dateOfBirth: "09 Jul 2004",
+    subject: "Subjects",
+    hobbies: [HOBBIES.sport, HOBBIES.music],
+    address: "Ukraine",
+    state: STATE.NCR,
+    city: CITY.Noida,
+  };
+  beforeEach(() => {
+    cy.visit("");
   });
 
-  it('', () => {
-
+  it("should have h1 item", () => {
+    cy.get("h1").should("contain.text", "Practice Form");
   });
+
+  it("should have h5 item", () => {
+    cy.get("h5").should("contain.text", "Student Registration Form");
+  });
+
+  it("should fill and submit form", () => {
+    cy.fillForm(user);
+    cy.get("#submit").click();
+    cy.get(".modal-dialog").should(
+      "contain.text",
+      "Thanks for submitting the form"
+    );
+  });
+
+  it(
+    "should open modal window with inputed data" + "after submitting the form",
+    () => {
+      cy.fillForm(user).then(() => {
+        cy.get("#submit").click();
+
+        cy.checkUser(user);
+      });
+    }
+  );
 });
