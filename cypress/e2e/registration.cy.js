f61668c8685aa0a3b3c11798eb0683f21163c51e
Roksolana Kalytyn <roksa.kalytyn@gmail.com>
2024-07-18 17:55:53 +0300

    Solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,7 +1,7 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('https://demoqa.com/automation-practice-form');
   });
 
@@ -9,10 +9,10 @@ describe('Student Registration page', () => {
     cy.get('.text-center')
       .should('contain', 'Practice Form');
 
-    cy.get('#firstName')
+    cy.get('[placeholder="First Name"]')
       .type('Ryan');
 
-    cy.get('#lastName')
+    cy.get('[placeholder="Last Name"]')
       .type('Gosling');
 
     cy.get('#userEmail')
@@ -20,40 +20,70 @@ describe('Student Registration page', () => {
 
     cy.get('label[for="gender-radio-1"]').click();
 
-    cy.get('#userNumber')
+    cy.get('[placeholder="Mobile Number"]')
       .type('0987654321');
 
     cy.get('#dateOfBirthInput').click();
 
     cy.get('.react-datepicker__month-select').select('November');
-
     cy.get('.react-datepicker__year-select').select('1980');
+    cy.get('[aria-label="Choose Wednesday, November 12th, 1980"]')
+      .click();
 
-    cy.get(':nth-child(3) > .react-datepicker__day--012').click();
-
-    cy.get('.subjects-auto-complete__value-container')
+    cy.get('#subjectsContainer')
       .type('comp{enter}');
 
     cy.get('label[for="hobbies-checkbox-1"]').click();
-
     cy.get('label[for="hobbies-checkbox-2"]').click();
-
     cy.get('label[for="hobbies-checkbox-3"]').click();
 
     cy.get('#currentAddress').type('My address');
 
-    cy.get('.css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder')
-      .click();
+    cy.get('#state').click();
+    cy.get('#react-select-3-option-1').click(); // Uttar Pradesh
 
-    cy.get('#react-select-3-option-1').click();
-
-    cy.get('.css-1wa3eu0-placeholder').click();
-
-    cy.get('#react-select-4-option-1').click();
+    cy.get('#city').click();
+    cy.get('#react-select-4-option-1').click(); // Lucknow
 
     cy.get('#submit').click();
 
-    cy.get('#example-modal-sizes-title-lg')
+    cy.get('.modal-header')
       .should('contain.text', 'Thanks for submitting the form');
+    cy.get('.table-responsive tbody tr').eq(0).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Student Name');
+      cy.get('td').eq(1).should('have.text', 'Ryan Gosling');
+    });
+    cy.get('.table-responsive tbody tr').eq(1).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Student Email');
+      cy.get('td').eq(1).should('have.text', 'fallguy24@gmail.com');
+    });
+    cy.get('.table-responsive tbody tr').eq(2).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Gender');
+      cy.get('td').eq(1).should('have.text', 'Male');
+    });
+    cy.get('.table-responsive tbody tr').eq(3).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Mobile');
+      cy.get('td').eq(1).should('have.text', '0987654321');
+    });
+    cy.get('.table-responsive tbody tr').eq(4).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Date of Birth');
+      cy.get('td').eq(1).should('have.text', '12 November,1980');
+    });
+    cy.get('.table-responsive tbody tr').eq(5).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Subjects');
+      cy.get('td').eq(1).should('have.text', 'Computer Science');
+    });
+    cy.get('.table-responsive tbody tr').eq(6).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Hobbies');
+      cy.get('td').eq(1).should('have.text', 'Sports, Reading, Music');
+    });
+    cy.get('.table-responsive tbody tr').eq(8).within(() => {
+      cy.get('td').eq(0).should('have.text', 'Address');
+      cy.get('td').eq(1).should('have.text', 'My address');
+    });
+    cy.get('.table-responsive tbody tr').eq(9).within(() => {
+      cy.get('td').eq(0).should('have.text', 'State and City');
+      cy.get('td').eq(1).should('have.text', 'Uttar Pradesh Lucknow');
+    });
   });
 });
