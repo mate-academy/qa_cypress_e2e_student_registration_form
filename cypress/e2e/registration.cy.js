7439dc5a703848a1d5c83790c124be94bd5ad441
yuliayahusta <yuliayahusta@gmail.com>
2024-07-29 10:05:47 +0300

    Fixed

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,24 +1,33 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
+  const firstName = 'Harry';
+  const lastName = 'Potter';
+  const email = 'HarryPotter@gmail.com';
+  const phoneNumber = '1234567890';
+  const birthYear = '1990';
+  const birthMonth = 'January';
+  const birthDay = '15';
+  const address = '567 London Ave, Derry, Maine';
+
   before(() => {
     cy.visit('https://demoqa.com/automation-practice-form');
   });
 
   it('should fill all required fields and submit the form', () => {
     // Filling required fields
-    cy.get('#firstName').type('Harry');
-    cy.get('#lastName').type('Potter');
-    cy.get('#userEmail').type('HarryPotter@gmail.com');
+    cy.get('#firstName').type(firstName);
+    cy.get('#lastName').type(lastName);
+    cy.get('#userEmail').type(email);
     cy.get('input[name="gender"][value="Male"]').click({ force: true });
-    cy.get('#userNumber').type('1234567890');
+    cy.get('#userNumber').type(phoneNumber);
     cy.get('#dateOfBirthInput').click();
-    cy.get('.react-datepicker__year-select').select('1990');
-    cy.get('.react-datepicker__month-select').select('January');
+    cy.get('.react-datepicker__year-select').select(birthYear);
+    cy.get('.react-datepicker__month-select').select(birthMonth);
     cy.get('.react-datepicker__day--015').click();
     cy.get('#subjectsInput').type('Maths{enter}');
     cy.get('input[type="checkbox"][value="2"]').click({ force: true });
-    cy.get('#currentAddress').type('567 London Ave, Derry, Maine');
+    cy.get('#currentAddress').type(address);
     cy.get('#state').click();
     cy.get('#react-select-3-option-0').click();
     cy.get('#city').click();
@@ -27,17 +36,15 @@ describe('Student Registration page', () => {
     cy.get('#submit').click();
 
     // Checking the entered data
-    cy.contains('td', 'Student Name').next().should('contain', 'Harry Potter');
-    cy.contains('td', 'Student Email')
-      .next().should('contain', 'HarryPotter@gmail.com');
+    cy.contains('td', 'Student Name').next().should('contain', `${firstName} ${lastName}`);
+    cy.contains('td', 'Student Email').next().should('contain', email);
     cy.contains('td', 'Gender').next().should('contain', 'Male');
-    cy.contains('td', 'Mobile').next().should('contain', '1234567890');
+    cy.contains('td', 'Mobile').next().should('contain', phoneNumber);
     cy.contains('td', 'Date of Birth')
-      .next().should('contain', '15 January,1990');
+      .next().should('contain', `${birthDay} ${birthMonth},${birthYear}`);
     cy.contains('td', 'Subjects').next().should('contain', 'Maths');
     cy.contains('td', 'Hobbies').next().should('contain', 'Reading');
-    cy.contains('td', 'Address')
-      .next().should('contain', '567 London Ave, Derry, Maine');
+    cy.contains('td', 'Address').next().should('contain', address);
     cy.contains('td', 'State and City').next().should('contain', 'NCR Delhi');
   });
 });
