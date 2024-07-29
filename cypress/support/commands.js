ca3017a34c3a4fbcf91c96aca32daf3396349003
Oleg Voloshyn <voloshyn.oleg.oleg@gmail.com>
2024-07-29 15:55:01 +0300

    home work

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,17 +9,19 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
+// // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
 //
-// -- This is a child command --
+// // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
+
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("pickDate", (data) => {
+  cy.get(`.react-datepicker__${data}`);
+});
