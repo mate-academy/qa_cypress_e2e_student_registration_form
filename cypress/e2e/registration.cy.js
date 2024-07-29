3fcc39b5545ba467e34bd84b93758b1425ac253e
yurayagu <yurayagu@gmail.com>
2024-07-29 17:03:42 +0300

    fixed

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,31 +1,35 @@
 /// <reference types='cypress' />
 
+import { faker } from '@faker-js/faker';
+
 describe('Student Registration page', () => {
-  const firstName = 'John';
-  const lastName = 'Coffey';
-  const email = 'JohnCoffey@gmail.com';
-  const phoneNumber = '1111111111';
-  const birthYear = '1957';
-  const birthMonth = 'December';
-  const birthDay = '15';
-  const address = '122 Block E, Cold Mountain, Louisiana';
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const email = faker.internet.email();
+  const phoneNumber = faker.string.numeric(10); // 10-digit number
+  const birthDate = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
+  const birthYear = birthDate.getFullYear();
+  const birthMonth = birthDate.toLocaleString('en-US', { month: 'long' });
+  const birthDay = String(birthDate.getDate()).padStart(2, '0');
+  const address = faker.location.streetAddress();
+
   before(() => {
     cy.visit('https://demoqa.com/automation-practice-form');
   });
 
-  it('', () => {
+  it('fills and submits the form with random data', () => {
     cy.get('#firstName').type(firstName);
     cy.get('#lastName').type(lastName);
     cy.get('#userEmail').type(email);
     cy.get('input[name="gender"][value="Male"]').click({ force: true });
     cy.get('#userNumber').type(phoneNumber);
     cy.get('#dateOfBirthInput').click();
-    cy.get('.react-datepicker__year-select').select(birthYear);
+    cy.get('.react-datepicker__year-select').select(birthYear.toString());
     cy.get('.react-datepicker__month-select').select(birthMonth);
-    cy.get('.react-datepicker__day--015').click();
+    cy.get(`.react-datepicker__day--0${birthDay}`).click();
     cy.get('#subjectsInput').type('Chemistry{enter}');
     cy.get('input[type="checkbox"][value="1"]').click({ force: true });
-    cy.get('#currentAddress').type(address);
+    cy.get('#currentAddress').type(address, { force: true });
     cy.get('#state').click();
     cy.get('#react-select-3-option-0').click();
     cy.get('#city').click();
