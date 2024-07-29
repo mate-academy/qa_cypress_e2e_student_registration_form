d6756a1ec3c954d93800ecae3b6c2e5868485e20
Maryna <marish1235@gmail.com>
2024-07-29 15:19:45 +0300

    My Registration page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('findByPlaceholder', (placeholder) => { 
+cy.get(`[placeholder="${placeholder}"]`);
+});
+
+Cypress.Commands.add('pickDate', (data) => { 
+cy.get(`.react-datepicker__${data}`);
+});
