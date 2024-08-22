2fefad37e4fa1fd7ae00f0d4624dadcec16bdf9d
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-08-22 13:24:06 +0300

    Registration Form

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
+    baseYrl:'https://demoqa.com',
+    viewportWidth: 500,
+    viewportHeight: 700,
     setupNodeEvents(on, config) {
-    }
-  }
-});
+      // implement node event listeners here
+    },
+  },
+};
