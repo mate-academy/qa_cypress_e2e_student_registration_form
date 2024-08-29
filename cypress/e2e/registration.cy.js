89a53e2d3254d70af3c48eb19bf2acb3e57dac78
Daryna Riabova <darinar474@gmail.com>
2024-08-29 14:49:11 +0200

    Fix all single quote errors

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -2,10 +2,28 @@
 
 describe('Student Registration page', () => {
   before(() => {
-
+    cy.visit('https://demoqa.com/automation-practice-form');
   });
+  const name = Math.random().toString();
+  const email = 'newemail@gmail.com';
+  const adress = 'Cracow. Pawia 101';
+  const number = '8777888666';
+  it('should fill all fields in forms except \'picture\'', () => {
+    cy.get('.practice-form-wrapper').within(() => {
+      cy.get('#firstName').type(name);
+      cy.get('#lastName').type(name);
+      cy.get('#userEmail').type(email);
+      cy.get('#genterWrapper > .col-md-9').click();
+      cy.get('#userNumber').type(number);
+      cy.get('#currentAddress').type(adress);
+      cy.get('#stateCity-wrapper > :nth-child(2)');
+      cy.get('#stateCity-wrapper > :nth-child(2)');
+      cy.get('#submit').click();
+    });
 
-  it('', () => {
-
+    cy.get('#adplus-anchor').then(($item) => {
+      $item.remove();
+    });
+    cy.get('#closeLargeModal').click();
   });
 });
