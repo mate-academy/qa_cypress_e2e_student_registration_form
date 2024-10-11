81f924989ac3fb6e2f8f1923e224e9f60e8c0f01
LyudmilaDidkivska <qawsed32@ukr.net>
2024-10-11 18:06:09 +0300

    Add Auto

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,53 @@
 /// <reference types='cypress' />
 
-describe('Student Registration page', () => {
-  before(() => {
-
+  Cypress.on('uncaught:exception', (err, runnable) => {
+   
+    return false;
   });
 
-  it('', () => {
+describe('Student Registration Form', () => {
 
+  before(() => {
+    cy.visit('https://demoqa.com/automation-practice-form');
+    cy.task('generateUser').then(generatedUser => {
+      user = generatedUser;
+    })
+  });
+
+  it('should fill the form and assert modal data', () => {
+    
+    cy.findByPlaceholderText('First Name').type(user.firstName);
+    cy.findByPlaceholderText('Last Name').type(user.lastName);
+    cy.get('#userEmail').type(user.email);
+    
+    
+    cy.get(`label[for="gender-radio-${user.genderId}"]`).click(); 
+    
+    
+    cy.get('#userNumber').type(user.phoneNumber);
+    cy.get('#dateOfBirthInput').click();
+    cy.get('.react-datepicker__year-select').select(user.birth.year);
+    cy.get('.react-datepicker__month-select').select(user.birth.month);
+    cy.get(`[aria-label="${user.birth.day}"]`).click();
+    cy.get(`label[for="hobbies-checkbox-${user.hobby}"]`).click();
+    cy.get('#currentAddress').type(user.address);
+    
+   
+    cy.get('#submit').click();
+    
+   
+
+    cy.get('.modal-body').should('contain', user.firstName);
+    cy.get('.modal-body').should('contain', user.lastName);
+    cy.get('.modal-body').should('contain', user.email);
+    cy.get('.modal-body').should('contain', user.phoneNumber);
+    cy.get('.modal-body').should('contain', user.birth.day);
+    cy.get('.modal-body').should('contain', user.birth.month);
+    cy.get('.modal-body').should('contain', user.birth.year);
+    cy.get('.modal-body').should('contain', user.hobby);
+    cy.get('.modal-body').should('contain', user.address);
+    
+    
+    cy.get('#closeLargeModal').click();
   });
 });
