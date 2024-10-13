0350e33153d053251a2e760ae577c3d39dcc3777
Svyatoslav <svyatko200311@gmail.com>
2024-10-13 19:19:13 +0300

    add test for reg page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,31 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const faker = require('@faker-js/faker').faker;
+const genders = ['Male', 'Female', 'Other'];
+const hobbies = ['Sports', 'Reading', 'Music'];
+const subjects = ['Math', 'Science', 'History'];
+
+function generateUser() {
+  const randomNumber = Math.round(Math.random() * 1000);
+  const username = `user${randomNumber}`;
+  const lastname = `user${randomNumber}${randomNumber}`;
+  const email = `${username}@mail.com`;
+  const phone = Math.round(Math.random() * 10000000000);
+  const randomIndexGender = Math.floor(Math.random() * genders.length);
+  const randomIndexHobby = Math.floor(Math.random() * hobbies.length);
+  const randomIndexSubject = Math.floor(Math.random() * subjects.length);
+  const address = faker.location.streetAddress();
+  return {
+    email,
+    username,
+    phone,
+    lastname,
+    address,
+    gender: genders[randomIndexGender],
+    hobby: hobbies[randomIndexHobby],
+    subject: subjects[randomIndexSubject]
+  };
+}
+
+module.exports = { generateUser };
