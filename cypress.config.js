05eeac38b5324465c2877000e1bd673ae8181de2
kaplunova <melamori1605@gmail.com>
2024-10-30 23:19:05 +0200

    added tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    }
+    },
+    baseUrl: 'https://demoqa.com',
+    defaultCommandTimeout: 100000
   }
 });
