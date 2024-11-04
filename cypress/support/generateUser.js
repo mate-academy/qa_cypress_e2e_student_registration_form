4e726caa934ff353cc4953e258b3ac9a801ddf9e
Vladislav <njiuyfqqq123@gmail.com>
2024-11-04 17:47:14 +0200

    done

---
+++ cypress/support/generateUser.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+const { faker } = require('@faker-js/faker')
+
+function generateUser() {
+  const userName = faker.internet.userName()
+  const email = faker.internet.email()
+  const mobile = Math.random().toString().slice(2, 12)
+  const subject = 'Maths'
+  const currentAddress = 'Volkova str 1'
+  const state = 'NCR'
+  const city = 'Noida'
+
+  return {
+    userName,
+    email,
+    mobile,
+    subject,
+    currentAddress,
+    state,
+    city,
+  }
+}
+
+module.exports = { generateUser }
