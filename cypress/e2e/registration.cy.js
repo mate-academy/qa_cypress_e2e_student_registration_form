3c38423befb928a37530023a24950c884a8c81b0
Nazar Bodak <nazarb@bookmap.com>
2025-01-26 20:30:20 +0200

    solution

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,16 @@
 /// <reference types='cypress' />
 
+const { generateUserData } = require('../support/generateUserData');
+
 describe('Student Registration page', () => {
   before(() => {
-
+    cy.visit('/');
   });
 
-  it('', () => {
-
+  it('should register user with valid data', () => {
+    const userData = generateUserData();
+    cy.fillRegistrationForm(userData);
+    cy.submitRegistrationForm();
+    cy.verifyUserData(userData);
   });
 });
